bb320ba22ddbfdfe88903838f3d26c6d7b56ec17
Anna Safyan <01forstudy@gmail.com>
2021-07-06 18:09:48 +0300

    fixed: fixed sintax mistakes

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,10 @@
+import re
+import sys
+import glob
+from time import localtime
+
 # 1 line: Output
-print('Hello, world!')
+print('Hello, world!\n')
 
 # 2 lines: Input, assignment
 name = input('What is your name?')
@@ -14,24 +19,26 @@ for i, name in enumerate(friends):
 parents, babies = (1, 1)
 while babies < 100:
     print('This generation has {0} babies'.format(babies))
-    parents, babies = (babies, parents+babies)
+    parents, babies = (babies, parents + babies)
+
 
 # 5 lines: Functions
 
 def greet(man):
-    print ('Hello', man)
+    print('Hello', man)
+
 
 greet('Jack')
 greet('Jill')
 greet('Bob')
 
-#6 lines: Import, regular expressions
-import re
+# 6 lines: Import, regular expressions
+
 for test_string in ['555-1212', 'ILL-EGAL']:
     if re.match(r'^\d{3}-\d{4}$', test_string):
-        print (test_string, 'is a valid US local phone number')
+        print(test_string, 'is a valid US local phone number')
     else:
-        print (test_string, 'rejected')
+        print(test_string, 'rejected')
 
 # 7 lines: Dictionaries, generator expressions
 prices = {'apple': 0.40, 'banana': 0.50}
@@ -39,47 +46,47 @@ my_purchase = {
     'apple': 1,
     'banana': 6}
 grocery_bill = sum(prices[fruit] * my_purchase[fruit] for fruit in my_purchase)
-print ('I owe the grocer $%.2f' % grocery_bill)
+print('I owe the grocer $%.2f' % grocery_bill)
 
-#8 lines: Command line arguments, exception handling
+# 8 lines: Command line arguments, exception handling
 # This program adds up integers that have been passed as arguments in the command line
-import sys
+
 try:
     total = sum(int(arg) for arg in sys.argv[1:])
-    print ('sum =', total)
+    print('sum =', total)
 except ValueError:
-    print ('Please supply integer arguments')
+    print('Please supply integer arguments')
 
-#9 lines: Opening files
+# 9 lines: Opening files
 # indent your Python code to put into an email
-import glob
+
+
 # glob supports Unix style pathname extensions
 python_files = glob.glob('*.py')
 for file_name in sorted(python_files):
-    print ('    ------' + file_name)
+    print('    ------' + file_name)
 
     with open(file_name) as f:
         for line in f:
-            print ('    ' + line.rstrip())
+            print('    ' + line.rstrip())
 
     print()
 
-#10 lines: Time, conditionals, from..import, for..else
-from time import localtime
+# 10 lines: Time, conditionals, from..import, for..else
 
 activities = {8: 'Sleeping',
               9: 'Commuting',
               17: 'Working',
               18: 'Commuting',
               20: 'Eating',
-              22: 'Resting' }
+              22: 'Resting'}
 
 time_now = localtime()
 hour = time_now.tm_hour
 
 for activity_time in sorted(activities.keys()):
     if hour < activity_time:
-        print (activities[activity_time])
+        print(activities[activity_time])
         break
 else:
-    print ('Unknown, AFK or sleeping!')
+    print('Unknown, AFK or sleeping!')
