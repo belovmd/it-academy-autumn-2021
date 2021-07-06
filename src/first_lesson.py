8c5ea6f242d2cc53c600758ece8241090ed8b63a
Яна Винничек <vinichek_yana@MacBook-Pro-Ana.local>
2021-07-06 23:30:22 +0300

    Python homework1

--- src/first_lesson.py
+++ src/first_lesson.py
@@ -1,45 +1,45 @@
-#print ('Hello, world!')
+print ('Hello, world!')
 
-#name = input('What is your name?\n')
-#print ('Hi, %s.' % name)
+name = input('What is your name?\n')
+print ('Hi, %s.' % name)
 
-#friends = ['john', 'pat', 'gary', 'michael']
-#for i, name in enumerate(friends):
-#    print ("iteration {iteration} is {name}".format(iteration=i, name=name))
+friends = ['john', 'pat', 'gary', 'michael']
+for i, name in enumerate(friends):
+    print ("iteration {iteration} is {name}".format(iteration=i, name=name))
 
-#parents, babies = (1, 1)
-#while babies < 100:
-#    print ('This generation has {0} babies'.format(babies))
-#    parents, babies = (babies, parents + babies)
+parents, babies = (1, 1)
+while babies < 100:
+    print ('This generation has {0} babies'.format(babies))
+    parents, babies = (babies, parents + babies)
 
-#def greet(name):
-#    print ('Hello', name)
-#greet('Jack')
-#greet('Jill')
-#greet('Bob')
+def greet(name):
+    print ('Hello', name)
+greet('Jack')
+greet('Jill')
+greet('Bob')
 
-#import re
-#for test_string in ['555-1212', 'ILL-EGAL']:
-#    if re.match(r'^\d{3}-\d{4}$', test_string):
-#        print (test_string, 'is a valid US local phone number')
-#    else:
-#        print (test_string, 'rejected')
+import re
+for test_string in ['555-1212', 'ILL-EGAL']:
+    if re.match(r'^\d{3}-\d{4}$', test_string):
+        print (test_string, 'is a valid US local phone number')
+    else:
+        print (test_string, 'rejected')
 
-#prices = {'apple': 0.40, 'banana': 0.50}
-#my_purchase = {
-#    'apple': 1,
-#    'banana': 6}
-#grocery_bill = sum(prices[fruit] * my_purchase[fruit]
-#                   for fruit in my_purchase)
-#print ('I owe the grocer $%.2f' % grocery_bill)
+prices = {'apple': 0.40, 'banana': 0.50}
+my_purchase = {
+    'apple': 1,
+    'banana': 6}
+grocery_bill = sum(prices[fruit] * my_purchase[fruit]
+                   for fruit in my_purchase)
+print ('I owe the grocer $%.2f' % grocery_bill)
 
 # This program adds up integers that have been passed as arguments in the command line
-#import sys
-#try:
-#    total = sum(int(arg) for arg in sys.argv[1:])
-#    print ('sum =', total)
-#except ValueError:
-#    print ('Please supply integer arguments')
+import sys
+try:
+    total = sum(int(arg) for arg in sys.argv[1:])
+    print ('sum =', total)
+except ValueError:
+    print ('Please supply integer arguments')
 
 # indent your Python code to put into an email
 import glob
@@ -51,3 +51,34 @@ for file_name in sorted(python_files):
         for line in f:
             print ('    ' + line.rstrip())
     print()
+
+from time import localtime
+
+activities = {8: 'Sleeping',
+              9: 'Commuting',
+              17: 'Working',
+              18: 'Commuting',
+              20: 'Eating',
+              22: 'Resting' }
+
+time_now = localtime()
+hour = time_now.tm_hour
+
+for activity_time in sorted(activities.keys()):
+    if hour < activity_time:
+        print (activities[activity_time])
+        break
+else:
+    print ('Unknown, AFK or sleeping!')
+
+REFRAIN = '''
+%d bottles of beer on the wall,
+%d bottles of beer,
+take one down, pass it around,
+%d bottles of beer on the wall!
+'''
+bottles_of_beer = 9
+while bottles_of_beer > 1:
+    print (REFRAIN % (bottles_of_beer, bottles_of_beer,
+        bottles_of_beer - 1))
+    bottles_of_beer -= 1
