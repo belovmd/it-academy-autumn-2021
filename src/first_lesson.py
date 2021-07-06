8dc7b653773c514472681ed81ade566a12e87220
Яна Винничек <vinichek_yana@MacBook-Pro-Ana.local>
2021-07-06 17:32:36 +0300

    Python homework 1

---
+++ src/first_lesson.py
@@ -0,0 +1,53 @@
+#print ('Hello, world!')
+
+#name = input('What is your name?\n')
+#print ('Hi, %s.' % name)
+
+#friends = ['john', 'pat', 'gary', 'michael']
+#for i, name in enumerate(friends):
+#    print ("iteration {iteration} is {name}".format(iteration=i, name=name))
+
+#parents, babies = (1, 1)
+#while babies < 100:
+#    print ('This generation has {0} babies'.format(babies))
+#    parents, babies = (babies, parents + babies)
+
+#def greet(name):
+#    print ('Hello', name)
+#greet('Jack')
+#greet('Jill')
+#greet('Bob')
+
+#import re
+#for test_string in ['555-1212', 'ILL-EGAL']:
+#    if re.match(r'^\d{3}-\d{4}$', test_string):
+#        print (test_string, 'is a valid US local phone number')
+#    else:
+#        print (test_string, 'rejected')
+
+#prices = {'apple': 0.40, 'banana': 0.50}
+#my_purchase = {
+#    'apple': 1,
+#    'banana': 6}
+#grocery_bill = sum(prices[fruit] * my_purchase[fruit]
+#                   for fruit in my_purchase)
+#print ('I owe the grocer $%.2f' % grocery_bill)
+
+# This program adds up integers that have been passed as arguments in the command line
+#import sys
+#try:
+#    total = sum(int(arg) for arg in sys.argv[1:])
+#    print ('sum =', total)
+#except ValueError:
+#    print ('Please supply integer arguments')
+
+# indent your Python code to put into an email
+import glob
+# glob supports Unix style pathname extensions
+python_files = glob.glob('*.py')
+for file_name in sorted(python_files):
+    print ('    ------' + file_name)
+    with open(file_name) as f:
+        for line in f:
+            print ('    ' + line.rstrip())
+    print()
