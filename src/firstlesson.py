957e1bd6fdcfd288ec61bb0e4864705a976f80de
kirerof <kirill.erofeev.07@gmail.com>
2021-07-07 00:51:13 +0300

    3 commit

--- src/firstlesson.py
+++ src/firstlesson.py
@@ -10,7 +10,7 @@ while babies < 100:
     parents, babies = (babies, parents + babies)
 
 
-def greet( name ):
+def greet(name):
     print('Hello', name)
 
 
@@ -18,6 +18,7 @@ greet('Jack')
 greet('Jill')
 greet('Bob')
 import re
+
 for test_string in ['555-1212', 'ILL-EGAL']:
     if re.match(r'^\d{3}-\d{4}$', test_string):
         print(test_string, 'is a valid US local phone number')
