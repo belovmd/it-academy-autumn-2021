3c74a936c070add63e1d1c551f26f8bab57a11f5
kirerof <kirill.erofeev.07@gmail.com>
2021-07-07 12:02:00 +0300

    5 commit

--- src/firstlesson.py
+++ src/firstlesson.py
@@ -1,4 +1,6 @@
 import re
+import sys
+import glob
 print('Hello, world!')
 name = input('What is your name?\n')
 print('Hi, %s.' % name)
@@ -32,3 +34,51 @@ my_purchase = {
 grocery_bill = sum(prices[fruit] * my_purchase[fruit]
                    for fruit in my_purchase)
 print('I owe the grocer $%.2f' % grocery_bill)
+# This program adds up integers that have been passed as arguments in the command line
+
+try:
+    total = sum(int(arg) for arg in sys.argv[1:])
+    print('sum =', total)
+except ValueError:
+    print('Please supply integer arguments')
+# indent your Python code to put into an email
+
+# glob supports Unix style pathname extensions
+python_files = glob.glob('*.py')
+for file_name in sorted(python_files):
+    print('    ------' + file_name)
+
+    with open(file_name) as f:
+        for line in f:
+            print('    ' + line.rstrip())
+
+    print()
+from time import localtime
+
+activities = {8: 'Sleeping',
+              9: 'Commuting',
+              17: 'Working',
+              18: 'Commuting',
+              20: 'Eating',
+              22: 'Resting' }
+
+time_now = localtime()
+hour = time_now.tm_hour
+
+for activity_time in sorted(activities.keys()):
+    if hour < activity_time:
+        print(activities[activity_time])
+        break
+else:
+    print('Unknown, AFK or sleeping!')
+REFRAIN = '''
+%d bottles of beer on the wall,
+%d bottles of beer,
+take one down, pass it around,
+%d bottles of beer on the wall!
+'''
+bottles_of_beer = 9
+while bottles_of_beer > 1:
+    print(REFRAIN % (bottles_of_beer, bottles_of_beer,
+        bottles_of_beer - 1))
+    bottles_of_beer -= 1
