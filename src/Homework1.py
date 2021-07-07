d405960d47be20a816b79a03f2c6aff4eee7f8a7
Julia Yurk <sovaf@ya.ru>
2021-07-07 19:08:49 +0300

    first task

---
+++ src/Homework1.py
@@ -0,0 +1,17 @@
+#task 1
+print ('Hello, world!')
+
+#task 2
+name = input('What is your name?\n')
+print ('Hi, %s.' % name)
+
+#task 3
+friends = ['john', 'pat', 'gary', 'michael']
+for i, name in enumerate(friends):
+    print ("iteration {iteration} is {name}".format(iteration=i, name=name))
+
+#task 4
+parents, babies = (1, 1)
+while babies < 100:
+    print ('This generation has {0} babies'.format(babies))
+    parents, babies = (babies, parents + babies)
