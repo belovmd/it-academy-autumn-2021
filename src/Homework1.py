64f021c4ee62ac94061b4f456643ee7822034f39
Julia Yurk <sovaf@ya.ru>
2021-07-07 22:19:12 +0300

    next task

--- src/Homework1.py
+++ src/Homework1.py
@@ -15,3 +15,22 @@ parents, babies = (1, 1)
 while babies < 100:
     print('This generation has {0} babies'.format(babies))
     parents, babies = (babies, parents + babies)
+
+# task 5
+
+
+def greet(name):
+    print('Hello', name)
+
+
+greet('Jack')
+greet('Jill')
+greet('Bob')
+
+# task 6
+import re
+for test_string in ['555-1212', 'ILL-LEGAL']:
+    if re.match(r'^\d{3}-\d{4}$', test_string):
+        print(test_string, 'is a valid US local phone number')
+    else:
+        print(test_string, 'rejected')
