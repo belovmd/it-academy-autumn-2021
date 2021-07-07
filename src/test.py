dce5fd68f42f7bb406aaa00c6a54f4b6a4913165
RamanSabeshkin <sabeshrom@gmail.com>
2021-07-07 22:16:06 +0200

    test4

--- src/test.py
+++ src/test.py
@@ -1,14 +1,14 @@
 #1 line: Output
-print ('Hello, world!')
+print('Hello, world!')
 
 #2 lines: Input, assignment
 name = input('What is your name?\n')
-print ('Hi, %s.' % name)
+print('Hi, %s.' % name)
 
 #3 lines: For loop, built-in enumerate function, new style formatting
 friends = ['john', 'pat', 'gary', 'michael']
 for i, name in enumerate(friends):
-    print ("iteration {iteration} is {name}".format(iteration=i, name=name))
+    print("iteration {iteration} is {name}".format(iteration=i, name=name))
 
 #4 lines: Fibonacci, tuple assignment
 parents, babies = (1, 1)
