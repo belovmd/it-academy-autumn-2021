9ef031d0c9701f3d273f7d39db074e3440a998f2
Julia Yurk <sovaf@ya.ru>
2021-07-07 20:05:28 +0300

    fix

--- src/Homework1.py
+++ src/Homework1.py
@@ -1,17 +1,17 @@
-#task 1
-print ('Hello, world!')
+# task 1
+print('Hello, world!')
 
-#task 2
+# task 2
 name = input('What is your name?\n')
-print ('Hi, %s.' % name)
+print('Hi, %s.' % name)
 
-#task 3
+# task 3
 friends = ['john', 'pat', 'gary', 'michael']
 for i, name in enumerate(friends):
-    print ("iteration {iteration} is {name}".format(iteration=i, name=name))
+    print("iteration {iteration} is {name}".format(iteration=i, name=name))
 
-#task 4
+# task 4
 parents, babies = (1, 1)
 while babies < 100:
-    print ('This generation has {0} babies'.format(babies))
+    print('This generation has {0} babies'.format(babies))
     parents, babies = (babies, parents + babies)
