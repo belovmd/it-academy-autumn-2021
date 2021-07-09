6442d4a8ea97a3e745838caba1ccccda03c5af85
RamanSabeshkin <sabeshrom@gmail.com>
2021-07-10 01:28:26 +0200

    test2

--- HomeworkTest.py
+++ HomeworkTest.py
@@ -1,44 +1,54 @@
-#1 line: Output
+import re
+import sys
+import glob
+from time import localtime
+import unittest
+from itertools import groupby
+import csv
+import itertools
 
 
-print ('Hello, world!')
-#2 lines: Input, assignment
+# 1 line: Output
+
+
+print('Hello, world!')
+# 2 lines: Input, assignment
 
 
 name = input('What is your name?\n')
-print ('Hi, %s.' % name)
-#3 lines: For loop, built-in enumerate function, new style formatting
+print('Hi, %s.' % name)
+# 3 lines: For loop, built-in enumerate function, new style formatting
 
 
 friends = ['john', 'pat', 'gary', 'michael']
 for i, name in enumerate(friends):
-    print ("iteration {iteration} is {name}".format(iteration=i, name=name))
-#4 lines: Fibonacci, tuple assignment
+    print("iteration {iteration} is {name}".format(iteration=i, name=name))
+# 4 lines: Fibonacci, tuple assignment
 
 
 parents, babies = (1, 1)
 while babies < 100:
-    print ('This generation has {0} babies'.format(babies))
+    print('This generation has {0} babies'.format(babies))
     parents, babies = (babies, parents + babies)
-#5 lines: Functions
+# 5 lines: Functions
 
 
 def greet(name):
-    print ('Hello', name)
+    print('Hello', name)
+
 
 greet('Jack')
 greet('Jill')
 greet('Bob')
-6 lines: Import, regular expressions
+# 6 lines: Import, regular expressions
 
 
-import re
 for test_string in ['555-1212', 'ILL-EGAL']:
     if re.match(r'^\d{3}-\d{4}$', test_string):
-        print (test_string, 'is a valid US local phone number')
+        print(test_string, 'is a valid US local phone number')
     else:
-        print (test_string, 'rejected')
-#7 lines: Dictionaries, generator expressions
+        print(test_string, 'rejected')
+# 7 lines: Dictionaries, generator expressions
 
 
 prices = {'apple': 0.40, 'banana': 0.50}
@@ -47,36 +57,34 @@ my_purchase = {
     'banana': 6}
 grocery_bill = sum(prices[fruit] * my_purchase[fruit]
                    for fruit in my_purchase)
-print ('I owe the grocer $%.2f' % grocery_bill)
-#8 lines: Command line arguments, exception handling
+print('I owe the grocer $%.2f' % grocery_bill)
+# 8 lines: Command line arguments, exception handling
 
 
 # This program adds up integers that have been passed as arguments in the command line
-import sys
+
 try:
     total = sum(int(arg) for arg in sys.argv[1:])
-    print ('sum =', total)
+    print('sum =', total)
 except ValueError:
-    print ('Please supply integer arguments')
-#9 lines: Opening files
+    print('Please supply integer arguments')
+# 9 lines: Opening files
 
 
 # indent your Python code to put into an email
-import glob
+
 # glob supports Unix style pathname extensions
 python_files = glob.glob('*.py')
 for file_name in sorted(python_files):
-    print ('    ------' + file_name)
+    print('    ------' + file_name)
 
     with open(file_name) as f:
         for line in f:
-            print ('    ' + line.rstrip())
+            print('    ' + line.rstrip())
 
     print()
-#10 lines: Time, conditionals, from..import, for..else
+# 1 0 lines: Time, conditionals, from..import, for..else
 
-
-from time import localtime
 
 activities = {8: 'Sleeping',
               9: 'Commuting',
@@ -90,10 +98,10 @@ hour = time_now.tm_hour
 
 for activity_time in sorted(activities.keys()):
     if hour < activity_time:
-        print (activities[activity_time])
+        print(activities[activity_time])
         break
 else:
-    print ('Unknown, AFK or sleeping!')
+    print('Unknown, AFK or sleeping!')
 #11 lines: Triple-quoted strings, while loop
 
 
@@ -105,11 +113,12 @@ take one down, pass it around,
 '''
 bottles_of_beer = 9
 while bottles_of_beer > 1:
-    print (REFRAIN % (bottles_of_beer, bottles_of_beer,
+    print(REFRAIN % (bottles_of_beer, bottles_of_beer,
         bottles_of_beer - 1))
     bottles_of_beer -= 1
-#12 lines: Classes
 
+
+# 12 lines: Classes
 
 class BankAccount(object):
     def __init__(self, initial_balance=0):
@@ -122,11 +131,11 @@ class BankAccount(object):
         return self.balance < 0
 my_account = BankAccount(15)
 my_account.withdraw(50)
-print (my_account.balance, my_account.overdrawn())
-#13 lines: Unit testing with unittest
+print(my_account.balance, my_account.overdrawn())
+# 13 lines: Unit testing with unittest
 
 
-import unittest
+
 def median(pool):
     copy = sorted(pool)
     size = len(copy)
@@ -134,12 +143,15 @@ def median(pool):
         return copy[int((size - 1) / 2)]
     else:
         return (copy[int(size/2 - 1)] + copy[int(size/2)]) / 2
+
 class TestMedian(unittest.TestCase):
     def testMedian(self):
         self.assertEqual(median([2, 9, 9, 7, 9, 2, 4, 5, 8]), 7)
+
 if __name__ == '__main__':
     unittest.main()
-#14 lines: Doctest-based testing
+
+# 14 lines: Doctest-based testing
 
 
 def median(pool):
@@ -153,13 +165,15 @@ def median(pool):
         return copy[int((size - 1) / 2)]
     else:
         return (copy[int(size/2 - 1)] + copy[int(size/2)]) / 2
+
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
-#15 lines: itertools
+
+# 15 lines: itertools
 
 
-from itertools import groupby
+
 lines = '''
 This is the
 first paragraph.
@@ -174,10 +188,10 @@ for has_chars, frags in groupby(lines, bool):
 # PRINTS:
 # This is the first paragraph.
 # This is the second.
-#16 lines: csv module, tuple unpacking, cmp() built-in
+# 16 lines: csv module, tuple unpacking, cmp() built-in
 
 
-import csv
+
 
 # need to define cmp function in Python 3
 def cmp(a, b):
@@ -200,7 +214,7 @@ with open('stocks.csv', 'r') as stocksFile:
     for ticker, name, price, change, pct in stocks:
         status = status_labels[cmp(float(change), 0.0)]
         print ('%s is %s (%.2f)' % (name, status, float(pct)))
-#18 lines: 8-Queens Problem (recursion)
+# 18 lines: 8-Queens Problem (recursion)
 
 
 BOARD_SIZE = 8
@@ -225,12 +239,12 @@ def solve(n):
         for i in range(BOARD_SIZE)
             for solution in smaller_solutions
                 if not under_attack(i+1, solution)]
+
 for answer in solve(BOARD_SIZE):
-    print (answer)
-#20 lines: Prime numbers sieve w/fancy generators
+    print(answer)
 
 
-import itertools
+# 20 lines: Prime numbers sieve w/fancy generators
 
 def iter_primes():
      # an iterator of all numbers between 2 and +infinity
@@ -249,8 +263,10 @@ def iter_primes():
 for p in iter_primes():
     if p > 1000:
         break
-    print (p)
-#21 lines: XML/HTML parsing
+    print(p)
+
+
+# 21 lines: XML/HTML parsing
 
 
 dinner_recipe = '''<html><body><table>
@@ -273,8 +289,8 @@ pantry = set(['olive oil', 'pesto'])
 for ingredient in tree.getiterator('tr'):
     amt, unit, item = ingredient
     if item.tag == "td" and item.text not in pantry:
-        print ("%s: %s %s" % (item.text, amt.text, unit.text))
-#28 lines: 8-Queens Problem (define your own exceptions)
+        print("%s: %s %s" % (item.text, amt.text, unit.text))
+# 28 lines: 8-Queens Problem (define your own exceptions)
 
 
 BOARD_SIZE = 8
@@ -305,7 +321,7 @@ def add_queen(queens):
 queens = add_queen([])
 print (queens)
 print ("\n".join(". "*q + "Q " + ". "*(BOARD_SIZE-q-1) for q in queens))
-#33 lines: "Guess the Number" Game (edited) from http://inventwithpython.com
+# 33 lines: "Guess the Number" Game (edited) from http://inventwithpython.com
 
 
 import random
@@ -315,7 +331,7 @@ guesses_made = 0
 name = input('Hello! What is your name?\n')
 
 number = random.randint(1, 20)
-print ('Well, {0}, I am thinking of a number between 1 and 20.'.format(name))
+print('Well, {0}, I am thinking of a number between 1 and 20.'.format(name))
 
 while guesses_made < 6:
 
@@ -324,15 +340,15 @@ while guesses_made < 6:
     guesses_made += 1
 
     if guess < number:
-        print ('Your guess is too low.')
+        print('Your guess is too low.')
 
     if guess > number:
-        print ('Your guess is too high.')
+        print('Your guess is too high.')
 
     if guess == number:
         break
 
 if guess == number:
-    print ('Good job, {0}! You guessed my number in {1} guesses!'.format(name, guesses_made))
+    print('Good job, {0}! You guessed my number in {1} guesses!'.format(name, guesses_made))
 else:
-    print ('Nope. The number I was thinking of was {0}'.format(number))
+    print('Nope. The number I was thinking of was {0}'.format(number))
