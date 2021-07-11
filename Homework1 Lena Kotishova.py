a18c5816050185d5b54eab5358bc20bc987d109f
lenakotishova <86805388+lenakotishova@users.noreply.github.com>
2021-07-11 13:09:13 +0300

    Update Homework1 Lena Kotishova.py

--- Homework1 Lena Kotishova.py
+++ Homework1 Lena Kotishova.py
@@ -1,21 +1,23 @@
 """task 1"""
-print ('Hello, world!')
+print('Hello, world!')
 """ task 2"""
 name = input('What is your name?\n')
-print ('Hi, %s.' % name)
+print('Hi, %s.' % name)
 """ task 3"""
 friends = ['john', 'pat', 'gary', 'michael']
 for i, name in enumerate(friends):
-    print ("iteration {iteration} is {name}".format(iteration=i, name=name))
+    print("iteration {iteration} is {name}".format(iteration=i, name=name))
 """task 4"""
 parents, babies = (1, 1)
 while babies < 100:
-    print ('This generation has {0} babies'.format(babies))
+    print('This generation has {0} babies'.format(babies))
     parents, babies = (babies, parents + babies)
 """task 5"""
 def greet(name):
-    print ('Hello', name)
-
+    
+    
+       print('Hello', name)
+        
 greet('Jack')
 greet('Jill')
 greet('Bob')
