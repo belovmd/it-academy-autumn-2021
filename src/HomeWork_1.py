6448ffc60f09ebb8d74cef43193422632e6aec6c
TatSmat <smatova.tatiana@gmail.com>
2021-07-12 12:10:30 +0300

    fixed

--- src/HomeWork_1.py
+++ src/HomeWork_1.py
@@ -1,33 +1,38 @@
+# 6 lines: Import, regular expressions
+import re
+
+for test_string in ['555-1212', 'ILLEGAL']:
+    if re.match(r'^\d{3}-\d{4}$', test_string):
+        print(test_string, 'is a valid US local phone number')
+    else:
+        print(test_string, 'rejected')
+
 # 1 line: Output
-print ('Hello, world!')
+print('Hello, world!')
 
 # 2 lines: Input, assignment
 name = input('What is your name?\n')
-print ('Hi, %s.' % name)
+print('Hi, %s.' % name)
 
 # 3 lines: For loop, built-in enumerate function, new style formatting
 friends = ['john', 'pat', 'gary', 'michael']
 for i, name in enumerate(friends):
-    print ("iteration {iteration} is {name}".format(iteration=i, name=name))
+    print("iteration {iteration} is {name}".format(iteration=i, name=name))
 
 # 4 lines: Fibonacci, tuple assignment
 parents, babies = (1, 1)
 while babies < 100:
-    print ('This generation has {0} babies'.format(babies))
+    print('This generation has {0} babies'.format(babies))
     parents, babies = (babies, parents + babies)
 
+
 # 5 lines: Functions
-def greet(name):
-    print ('Hello', name)
+
+
+def greet(name1):
+    print('Hello', name1)
+
 
 greet('Jack')
 greet('Jill')
 greet('Bob')
-
-# 6 lines: Import, regular expressions
-import re
-for test_string in ['555-1212', 'ILL-EGAL']:
-    if re.match(r'^\d{3}-\d{4}$', test_string):
-        print (test_string, 'is a valid US local phone number')
-    else:
-        print (test_string, 'rejected')
