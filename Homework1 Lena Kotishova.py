452e2d56b72b826a1aa2d7aea867f004b682f7b1
lenakotishova <86805388+lenakotishova@users.noreply.github.com>
2021-07-12 17:56:29 +0300

    add file

--- Homework1 Lena Kotishova.py
+++ Homework1 Lena Kotishova.py
@@ -2,31 +2,34 @@ print('Введите цену, которую Вы заплатили, за у�
 M = int(input('рублей: '))
 N = int(input('копеек: '))
 S = int(input('Введите приобретенное количество товара: '))
-Rprice = M/S  # цена одной штуки в рублях
-Kprice = N/S  # цена одной штуки в копейках
+Rprice = M / S  # цена одной штуки в рублях
+Kprice = N / S  # цена одной штуки в копейках
 print('Цена за одну единицу составила: ', int(Rprice), 'рублей', int(Kprice), 'копеек')
 L = int(input('Пожалуйста, введите желаемое количество товара: '))
-N1 = N/100  # N1 - рассчитала копейки, чтобы сделать одно целое число
-price = M+N1  #сделала целое число
-total = price/S*L
-print('Общая цена товара за желаемое количество товаров составит: ', total,  'рублей')
+N1 = N / 100  # N1 - рассчитала копейки, чтобы сделать одно целое число
+price = M + N1  #сделала целое число
+total = price / S * L
+print('Общая цена товара за желаемое количество товаров составит: ', total, 'рублей')
 """ task 1"""
 print('Hello, world!')
 """ task 2"""
 name = input('What is your name?\n')
-print ('Hi, %s.' % name)
+print('Hi, %s.' % name)
 """ task 3"""
 friends = ['john', 'pat', 'gary', 'michael']
 for i, name in enumerate(friends):
-    print ("iteration {iteration} is {name}".format(iteration=i, name=name))
+    print("iteration {iteration} is {name}".format(iteration=i, name=name))
 """task 4"""
 parents, babies = (1, 1)
 while babies < 100:
-    print ('This generation has {0} babies'.format(babies))
+    print('This generation has {0} babies'.format(babies))
     parents, babies = (babies, parents + babies)
 """task 5"""
+
+
 def greet(name):
-    print ('Hello', name)
+    print('Hello', name)
+
 
 greet('Jack')
 greet('Jill')
