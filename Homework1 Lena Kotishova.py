59d50a5520491196e950b2d261d7ed7a0a470bde
lenakotishova <86805388+lenakotishova@users.noreply.github.com>
2021-07-12 18:13:19 +0300

    add file

--- Homework1 Lena Kotishova.py
+++ Homework1 Lena Kotishova.py
@@ -7,7 +7,7 @@ Kprice = N / S  # цена одной штуки в копейках
 print('Цена за одну единицу составила: ', int(Rprice), 'рублей', int(Kprice), 'копеек')
 L = int(input('Пожалуйста, введите желаемое количество товара: '))
 N1 = N / 100  # N1 - рассчитала копейки, чтобы сделать одно целое число
-price = M + N1  #сделала целое число
+price = M + N1  # сделала целое число
 total = price / S * L
 print('Общая цена товара за желаемое количество товаров составит: ', total, 'рублей')
 """ task 1"""
@@ -38,9 +38,9 @@ greet('Bob')
 import re
 for test_string in ['555-1212', 'ILL-EGAL']:
     if re.match(r'^\d{3}-\d{4}$', test_string):
-        print (test_string, 'is a valid US local phone number')
+        print(test_string, 'is a valid US local phone number')
     else:
-        print (test_string, 'rejected')
+        print(test_string, 'rejected')
 """task 7"""
 prices = {'apple': 0.40, 'banana': 0.50}
 my_purchase = {
@@ -48,13 +48,14 @@ my_purchase = {
     'banana': 6}
 grocery_bill = sum(prices[fruit] * my_purchase[fruit]
                    for fruit in my_purchase)
-print ('I owe the grocer $%.2f' % grocery_bill)
+print('I owe the grocer $%.2f' % grocery_bill)
 """task 8"""
 # This program adds up integers that have been passed as arguments in the command line
-import sys
 try:
+    import sys
     total = sum(int(arg) for arg in sys.argv[1:])
-    print ('sum =', total)
+    print('sum =', total)
+
 except ValueError:
     print ('Please supply integer arguments')
 """task 9"""
