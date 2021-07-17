61c7f2bdd16c4fba1d029fbe054f4cda324bbb50
TatSmat <smatova.tatiana@gmail.com>
2021-07-17 16:46:40 +0300

    Added some tasks more. Formatted in following view: 1 line between docstring and code and 2 lines between last line of previous task and the docstring of the next task

--- src/HomeWork_1.py
+++ src/HomeWork_1.py
@@ -1,33 +1,258 @@
+import re
+
+import sys
+
+import glob
+
+import unittest
+
+import csv
+
+from itertools import groupby
+
+from time import localtime
+
 # 1 line: Output
-print ('Hello, world!')
+
+print('Hello, world!')
 
 # 2 lines: Input, assignment
+
 name = input('What is your name?\n')
-print ('Hi, %s.' % name)
+print('Hi, %s.' % name)
 
 # 3 lines: For loop, built-in enumerate function, new style formatting
+
 friends = ['john', 'pat', 'gary', 'michael']
 for i, name in enumerate(friends):
-    print ("iteration {iteration} is {name}".format(iteration=i, name=name))
+    print("iteration {iteration} is {name}".format(iteration=i, name=name))
 
 # 4 lines: Fibonacci, tuple assignment
+
 parents, babies = (1, 1)
 while babies < 100:
-    print ('This generation has {0} babies'.format(babies))
+    print('This generation has {0} babies'.format(babies))
     parents, babies = (babies, parents + babies)
 
+
 # 5 lines: Functions
-def greet(name):
-    print ('Hello', name)
+
+def greet(name1):
+    print('Hello', name1)
+
 
 greet('Jack')
 greet('Jill')
 greet('Bob')
 
 # 6 lines: Import, regular expressions
-import re
-for test_string in ['555-1212', 'ILL-EGAL']:
+
+for test_string in ['555-1212', 'ILLEGAL']:
     if re.match(r'^\d{3}-\d{4}$', test_string):
-        print (test_string, 'is a valid US local phone number')
+        print(test_string, 'is a valid US local phone number')
     else:
-        print (test_string, 'rejected')
+        print(test_string, 'rejected')
+
+# 7 lines: Dictionaries, generator expressions
+
+prices = {'apple': 0.40, 'banana': 0.50}
+my_purchase = {
+    'apple': 1,
+    'banana': 6}
+grocery_bill = sum(prices[fruit] * my_purchase[fruit]
+                   for fruit in my_purchase)
+print('I owe the grocer $%.2f' % grocery_bill)
+
+# 8 lines: Command line arguments, exception handling
+# This program adds up integers that have been passed as arguments in the command line
+
+try:
+    total = sum(int(arg) for arg in sys.argv[1:])
+    print('sum =', total)
+except ValueError:
+    print('Please supply integer arguments')
+
+# 9 lines: Opening files
+# indent your Python code to put into an email
+# glob supports Unix style pathname extensions
+
+python_files = glob.glob('*.py')
+for file_name in sorted(python_files):
+    print('    ------' + file_name)
+
+    with open(file_name) as f:
+        for line in f:
+            print('    ' + line.rstrip())
+
+    print()
+
+# 10 lines: Time, conditionals, from..import, for..else
+
+activities = {8: 'Sleeping',
+              9: 'Commuting',
+              17: 'Working',
+              18: 'Commuting',
+              20: 'Eating',
+              22: 'Resting'}
+
+time_now = localtime()
+hour = time_now.tm_hour
+
+for activity_time in sorted(activities.keys()):
+    if hour < activity_time:
+        print(activities[activity_time])
+        break
+else:
+    print('Unknown, AFK or sleeping!')
+
+# 11 lines: Triple-quoted strings, while loop
+
+REFRAIN = '''
+%d bottles of beer on the wall,
+%d bottles of beer,
+take one down, pass it around,
+%d bottles of beer on the wall!
+'''
+bottles_of_beer = 9
+while bottles_of_beer > 1:
+    print(REFRAIN % (bottles_of_beer, bottles_of_beer,
+                     bottles_of_beer - 1))
+    bottles_of_beer -= 1
+
+
+# 12 lines: Classes
+
+class BankAccount(object):
+    def __init__(self, initial_balance=0):
+        self.balance = initial_balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        self.balance -= amount
+
+    def overdrawn(self):
+        return self.balance < 0
+
+
+my_account = BankAccount(15)
+my_account.withdraw(50)
+print(my_account.balance, my_account.overdrawn())
+
+
+# 13 lines: Unit testing with unittest
+
+def median(pool):
+    copy = sorted(pool)
+    size = len(copy)
+    if size % 2 == 1:
+        return copy[int((size - 1) / 2)]
+    else:
+        return (copy[int(size / 2 - 1)] + copy[int(size / 2)]) / 2
+
+
+class TestMedian(unittest.TestCase):
+    def testMedian(self):
+        self.assertEqual(median([2, 9, 9, 7, 9, 2, 4, 5, 8]), 7)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+# 14 lines: Doctest-based testing
+
+def median(pool):
+    """Statistical median to demonstrate doctest.
+    >>> median([2, 9, 9, 7, 9, 2, 4, 5, 8])
+    6 #change to 7 in order to pass the test
+    """
+    copy = sorted(pool)
+    size = len(copy)
+    if size % 2 == 1:
+        return copy[int((size - 1) / 2)]
+    else:
+        return (copy[int(size / 2 - 1)] + copy[int(size / 2)]) / 2
+
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod()
+
+# 15 lines: itertools
+
+lines = '''
+This is the
+first paragraph.
+
+This is the second.
+'''.splitlines()
+# Use itertools.group by and bool to return groups of
+# consecutive lines that either have content or don't.
+for has_chars, frags in groupby(lines, bool):
+    if has_chars:
+        print(' '.join(frags))
+
+
+# PRINTS:
+# This is the first paragraph.
+# This is the second.
+
+
+# 16 lines: csv module, tuple unpacking, cmp() built-in
+# need to define cmp function in Python 3
+
+def cmp(a, b):
+    return (a > b) - (a < b)
+
+
+# write stocks data as comma-separated values
+with open('stocks.csv', 'w', newline='') as stocksFileW:
+    writer = csv.writer(stocksFileW)
+    writer.writerows([
+        ['GOOG', 'Google, Inc.', 505.24, 0.47, 0.09],
+        ['YHOO', 'Yahoo! Inc.', 27.38, 0.33, 1.22],
+        ['CNET', 'CNET Networks, Inc.', 8.62, -0.13, -1.4901]
+    ])
+
+# read stocks data, print status messages
+with open('stocks.csv', 'r') as stocksFile:
+    stocks = csv.reader(stocksFile)
+
+    status_labels = {-1: 'down', 0: 'unchanged', 1: 'up'}
+    for ticker, name, price, change, pct in stocks:
+        status = status_labels[cmp(float(change), 0.0)]
+        print('%s is %s (%.2f)' % (name, status, float(pct)))
+
+# 18 lines: 8-Queens Problem (recursion)
+
+BOARD_SIZE = 8
+
+
+def under_attack(col, queens):
+    left = right = col
+
+    for r, c in reversed(queens):
+        left, right = left - 1, right + 1
+
+        if c in (left, col, right):
+            return True
+    return False
+
+
+def solve(n):
+    if n == 0:
+        return [[]]
+
+    smaller_solutions = solve(n - 1)
+
+    return [solution + [(n, _i + 1)]
+            for _i in range(BOARD_SIZE)
+            for solution in smaller_solutions
+            if not under_attack(_i + 1, solution)]
+
+
+for answer in solve(BOARD_SIZE):
+    print(answer)
