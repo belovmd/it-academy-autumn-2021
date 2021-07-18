da99005a3c7fe32aa72292fe404d6d1b3147c9bb
raivania <le.pzzbe@gmail.com>
2021-07-18 22:39:54 +0300

    commit

--- src/homework2/task6.py
+++ src/homework2/task6.py
@@ -1,1 +1,11 @@
-addwda
+num = int(input('Введите число число: '))
+rev = 0
+n = num
+while n:
+    last_num = n % 10
+    rev = rev * 10 + last_num
+    n = (n // 10)
+if num == rev:
+    print("Палиндром")
+else:
+    print("Не палиндром")
