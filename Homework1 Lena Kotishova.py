7fb688cc6f35223937c86ed45e2304837b413107
lenakotishova <86805388+lenakotishova@users.noreply.github.com>
2021-07-18 22:49:17 +0300

    add file

--- Homework1 Lena Kotishova.py
+++ Homework1 Lena Kotishova.py
@@ -1,84 +1,76 @@
-''' Task from  the lesson '''
-print('Введите цену, которую Вы заплатили, за указанное количество товаров: ')
+''' Task from the lesson '''
+print('Пожалуйста, введите цену одной вещи ниже:')
 M = int(input('рублей: '))
 N = int(input('копеек: '))
-S = int(input('Введите приобретенное количество товара: '))
-Rprice = M / S  # цена одной штуки в рублях
-Kprice = N / S  # цена одной штуки в копейках
-print('Цена за одну единицу составила: ', int(Rprice), 'рублей', int(Kprice), 'копеек')
-L = int(input('Пожалуйста, введите желаемое количество товара: '))
-N1 = N / 100  # N1 - рассчитала копейки, чтобы сделать одно целое число
-price = M + N1  # сделала целое число
-total = price / S * L
-print('Общая цена товара за желаемое количество товаров составит: ', total, 'рублей')
-
-
-""" task 1"""
-print('Hello, world!')
-
-
-""" task 2"""
+S = int(input('Введите желаемое количество товара: '))
+print('Цена одной Вашей вещи составила {0} рубля {1} копеек, посчитать {2} предмета'.format(M, N, S))
+total_rubles = M * S
+total_kopeck = N * S
+while total_kopeck >= 100:
+    total_kopeck = total_kopeck - 100
+    total_rubles = total_rubles + 1
+else:
+    pass
+print('Общая цена составит {} рублей {} копеек'.format(total_rubles, total_kopeck))  # last line of previous task
+# next task docstring
+pass  # my next task first line
+print('Hello, world!')  # last line of previous task
+# next task docstring
+pass  # my next task first line
 name = input('What is your name?\n')
 print('Hi, %s.' % name)
-
-
-""" task 3"""
+# last line of previous task
+# next task docstring
+pass  # my next task first line
 friends = ['john', 'pat', 'gary', 'michael']
 for i, name in enumerate(friends):
-    print("iteration {iteration} is {name}".format(iteration=i, name=name))
-
-
-"""task 4"""
+    print("iteration {iteration} is {name}".format(iteration=i, name=name))  # last line of previous task
+# next task docstring
+pass  # my next task first line
 parents, babies = (1, 1)
 while babies < 100:
     print('This generation has {0} babies'.format(babies))
-    parents, babies = (babies, parents + babies)
-
-
-"""task 5"""
-
-
+    parents, babies = (babies, parents + babies)  # last line of previous task
+# next task docstring
+pass  # my next task first line
 def greet(name):
     print('Hello', name)
 
 
 greet('Jack')
 greet('Jill')
-greet('Bob')
-
-
-"""task 6"""
+greet('Bob')  # last line of previous task
+# next task docstring
+pass  # my next task first line
 for test_string in ['555-1212', 'ILL-EGAL']:
     import re
+
     if re.match(r'^\d{3}-\d{4}$', test_string):
         print(test_string, 'is a valid US local phone number')
     else:
-        print(test_string, 'rejected')
-
-
-"""task 7"""
+        print(test_string, 'rejected')# last line of previous task
+# next task docstring
+pass  # my next task first line
 prices = {'apple': 0.40, 'banana': 0.50}
 my_purchase = {
     'apple': 1,
     'banana': 6}
 grocery_bill = sum(prices[fruit] * my_purchase[fruit]
                    for fruit in my_purchase)
-print('I owe the grocer $%.2f' % grocery_bill)
-
-
-"""task 8"""
+print('I owe the grocer $%.2f' % grocery_bill) # last line of previous task
+# next task docstring
+pass  # my next task first line
 # This program adds up integers that have been passed as arguments in the command line
 try:
     import sys
+
     total = sum(int(arg) for arg in sys.argv[1:])
     print('sum =', total)
 
 except ValueError:
-    print('Please supply integer arguments')
-
-
-"""task 10"""
-
+    print('Please supply integer arguments') # last line of previous task
+# next task docstring
+pass  # my next task first line
 BOARD_SIZE = 8
 
 
