dc34452d3d8ddd39e75e3f89dbd2b362f5f64ffc
RamanSabeshkin <sabeshrom@gmail.com>
2021-07-18 16:56:44 +0200

    test2

--- src/Task3.py
+++ src/Task3.py
@@ -1,21 +1,38 @@
 import re
 
 
-def remove_duplicate():
-    text = input("Введите выражение")
-    text = re.sub(r'[^а-яА-Я\w]', '', text)
-    text = text.replace(" ", "")
+"""Вводится строка. Требуется удалить из нее повторяющиеся символы и все
+    пробелы. Например, если было введено "abc cde def", то должно быть
+    выведено "abcdef".
+   Подсказка: оператор in проверяет, входит ли подстрока в строку или нет.
+"""
+
+
+def sub_string(str_):
+    """Конструирование подстроки.
+    :param str_: входная строка
+    :return: строка. Получившееся выражение
+    """
+
+    # Регуляркой заменяем все символы кроме кирилицы, латиницы и цифр на пустой символ ''
+    str_ = input("Введите выражение:")
+    text = re.sub(r'[^а-яА-Я\w]', '', str_)
     text = list(text)
 
+    # Создаем лист, в который добавляем символ, если его там нет,
+    # повторяющийся символ не добавляется
     new_list = []
     for char in text:
         if char not in new_list:
             new_list.append(char)
 
+    # Создаем строку, в которую добавляем символы из листа
     str_output = ""
     str_output = str_output.join(new_list)
-    print(str_output)
+    return str_output
 
 
 if __name__ == '__main__':
-    remove_duplicate()
+    # здесь можно сделать ввод из консоли и проверить работу функции
+    str_ = ''
+    print(sub_string(str_))
