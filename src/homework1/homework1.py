aaf023034f93c1a6b0b6b3507758361679854bd1
EvgenyLav <evgenlav1993@gmail.com>
2021-07-19 10:45:38 +0300

    fix

--- src/homework1/homework1.py
+++ src/homework1/homework1.py
@@ -1,83 +1,90 @@
+# 6 task first line
 import re
 
+# 7 task first line
+import sys
+
+# 8 task firs line
+import glob
+
+
+""" 1 line: Output"""
+
 print('Hello, world!')
+
+"""2 lines: Input, assignment"""
+
 name = input('What is your name?\n')
 print('Hi, %s.' % name)
+
+"""3 lines: For loop, built-in enumerate function, new style formatting"""
+
 friends = ['john', 'pat', 'gary', 'michael']
 for i, name in enumerate(friends):
     print("iteration {iteration} is {name}".format(iteration=i, name=name))
+
+"""4 lines: Fibonacci, tuple assignment"""
+
 parents, babies = (1, 1)
 while babies < 100:
     print('This generation has {0} babies'.format(babies))
     parents, babies = (babies, parents + babies)
 
+"""5 lines: Functions"""
 
-def greet(a):
-    print('Hello', a)
 
-from time import localtime
+def greet(fame):
+    print('Hello', fame)
 
-activities = {8: 'Sleeping',
-              9: 'Commuting',
-              17: 'Working',
-              18: 'Commuting',
-              20: 'Eating',
-              22: 'Resting' }
 
-time_now = localtime()
-hour = time_now.tm_hour
-
-for activity_time in sorted(activities.keys()):
-    if hour < activity_time:
-        print (activities[activity_time])
-        break
-else:
-    print ('Unknown, AFK or sleeping!')
 greet('Jack')
 greet('Jill')
 greet('Bob')
 
-for test_string in ['555-1212', 'ILL-LEGAL']:
-    if not re.match(r'^\d{3}-\d{4}$', test_string):
-        print(test_string, 'rejected')
-    else:
+"""6 lines: Import, regular expressions"""
+
+
+for test_string in ['555-1212', 'ILL-EGAL']:
+    if re.match(r'^\d{3}-\d{4}$', test_string):
         print(test_string, 'is a valid US local phone number')
-
-REFRAIN = '''
-%d bottles of beer on the wall,
-%d bottles of beer,
-take one down, pass it around,
-%d bottles of beer on the wall!
-'''
-bottles_of_beer = 9
-while bottles_of_beer > 1:
-    print (REFRAIN % (bottles_of_beer, bottles_of_beer,
-        bottles_of_beer - 1))
-    bottles_of_beer -= 1
-
-class BankAccount(object):
-    def __init__(self, initial_balance=0):
-        self.balance = initial_balance
-    def deposit(self, amount):
-        self.balance += amount
-    def withdraw(self, amount):
-        self.balance -= amount
-    def overdrawn(self):
-        return self.balance < 0
-my_account = BankAccount(15)
-my_account.withdraw(50)
-print (my_account.balance, my_account.overdrawn())
-
-import unittest
-def median(pool):
-    copy = sorted(pool)
-    size = len(copy)
-    if size % 2 == 1:
-        return copy[int((size - 1) / 2)]
     else:
-        return (copy[int(size/2 - 1)] + copy[int(size/2)]) / 2
-class TestMedian(unittest.TestCase):
-    def testMedian(self):
-        self.assertEqual(median([2, 9, 9, 7, 9, 2, 4, 5, 8]), 7)
-if __name__ == '__main__':
-    unittest.main()
+        print(test_string, 'rejected')
+
+for test_string in ['555-1212', 'ILL-EGAL']:
+    if re.match(r'^\d{3}-\d{4}$', test_string):
+        print(test_string, 'is a valid US local phone number')
+    else:
+        print(test_string, 'rejected')
+
+"""7 lines: Dictionaries, generator expressions"""
+
+prices = {'apple': 0.40, 'banana': 0.50}
+my_purchase = {
+    'apple': 1,
+    'banana': 6}
+grocery_bill = sum(prices[fruit] * my_purchase[fruit]
+                   for fruit in my_purchase)
+print('I owe the grocer $%.2f' % grocery_bill)
+
+"""8 lines: Command line arguments, exception handling"""
+
+# This program adds up integers that have been passed as arguments in the command line
+
+try:
+    total = sum(int(arg) for arg in sys.argv[1:])
+    print('sum =', total)
+except ValueError:
+    print('Please supply integer arguments')
+
+"""9 lines: Opening files"""
+
+# indent your Python code to put into an email
+python_files = glob.glob('*.py')
+for file_name in sorted(python_files):
+    print('    ------' + file_name)
+
+    with open(file_name) as f:
+        for line in f:
+            print('    ' + line.rstrip())
+
+    print()
