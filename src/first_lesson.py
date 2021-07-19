d2fa148b48ee9e365a4356df7cee4901f2bc7c50
Vinichek <Vinichek_Yana@mail.ru>
2021-07-19 17:17:51 +0300

    my homework1

--- src/first_lesson.py
+++ src/first_lesson.py
@@ -3,36 +3,49 @@ import re
 import sys
 from time import localtime
 
+
+# prints Hello World!
 print('Hello, world!')
 
+
+# prints the given name
 name = input('What is your name?\n')
 print('Hi, %s.' % name)
 
+
+# iterates over given array and prints every item according in sepecific format
 friends = ['john', 'pat', 'gary', 'michael']
 for i, name in enumerate(friends):
     print("iteration {iteration} is {name}".format(iteration=i, name=name))
 
+
+# simple usage of While-loop
 parents, babies = (1, 1)
 while babies < 100:
     print('This generation has {0} babies'.format(babies))
     parents, babies = (babies, parents + babies)
 
 
+# simple function definition
 def greet(name):
     print('Hello', name)
 
 
+# call the function with different arguments
 greet('Jack')
 greet('Jill')
 greet('Bob')
 
 
+# simple For-loop
 for test_string in ['555-1212', 'ILL-EGAL']:
     if re.match(r'^\d{3}-\d{4}$', test_string):
         print(test_string, 'is a valid US local phone number')
     else:
         print(test_string, 'rejected')
 
+
+# simple usage of an associative array
 prices = {'apple': 0.40, 'banana': 0.50}
 my_purchase = {
     'apple': 1,
@@ -42,15 +55,14 @@ grocery_bill = sum(prices[fruit] * my_purchase[fruit]
 print('I owe the grocer $%.2f' % grocery_bill)
 
 # This program adds up integers that have been passed as arguments in the command line
-
 try:
     total = sum(int(arg) for arg in sys.argv[1:])
     print('sum =', total)
 except ValueError:
     print('Please supply integer arguments')
 
-# indent your Python code to put into an email
 
+# indent your Python code to put into an email
 # glob supports Unix style pathname extensions
 python_files = glob.glob('*.py')
 for file_name in sorted(python_files):
@@ -60,6 +72,8 @@ for file_name in sorted(python_files):
             print('    ' + line.rstrip())
     print()
 
+
+# For-loop + associative array
 activities = {8: 'Sleeping',
               9: 'Commuting',
               17: 'Working',
@@ -77,6 +91,8 @@ for activity_time in sorted(activities.keys()):
 else:
     print('Unknown, AFK or sleeping!')
 
+
+# simple While-loop + formatted strings
 REFRAIN = '''
 %d bottles of beer on the wall,
 %d bottles of beer,
