2a9f5adc87d9a62d096be5e3970c4d26c8024aa1
lenakotishova <86805388+lenakotishova@users.noreply.github.com>
2021-07-24 23:16:10 +0300

    Revert "task1"
    This reverts commit 87b06802686526eda61da9d9503e2dc766bc230a.
    
    # Conflicts:
    #	src/homework2/task1.py
    
    This was added by mistake, I am doing my best to delete this commit, however, haven't succeeded yet

--- src/homework2/task6.py
+++ src/homework2/task6.py
@@ -1,16 +1,17 @@
-'''6. Определите, является ли число палиндромом (читается слева направо и справа налево одинаково).
-Число положительное целое, произвольной длины. Задача требует работать только с числами
+'''6. Определите, является ли число палиндромом
+(читается слева направо и справа налево одинаково).
+Число положительное целое, произвольной длины.
+Задача требует работать только с числами
 (без конвертации числа в строку или что-нибудь еще)
 '''
 digits = int(input("Please, input the number for checking: "))
 a = int(digits)
 reverse = 0
 while digits != 0:
-    last_digit = digits % 10
-    reverse = reverse * 10 + last_digit  # mirroring the input number
-    digits = int(digits / 10)
-print(reverse)  # checking purposes again
+   last_digit = digits % 10
+   reverse = reverse * 10 + last_digit  # mirroring the input number
+   digits = int(digits / 10)
 if a == reverse:
-    print("Yes, that's a palindrome")
+   print("Yes, that's a palindrome")
 else:
-    print("No, it's not a palindrome")
+   print("No, it's not a palindrome")
