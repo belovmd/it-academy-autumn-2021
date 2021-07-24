62a8c22b3cb19194a09504223a0e0e0384ef5d0b
lenakotishova <86805388+lenakotishova@users.noreply.github.com>
2021-07-24 23:03:39 +0300

    Revert "task1"
    This reverts commit 87b06802686526eda61da9d9503e2dc766bc230a.
    
    # Conflicts:
    #	src/homework2/task1.py
    
    This was added by mistake, I am doing my best to delete this commit, however, haven't succeeded yet

--- src/homework2/task4.py
+++ src/homework2/task4.py
@@ -1,13 +1,17 @@
-'''Посчитать количество строчных (маленьких) и прописных (больших) букв в введенной строке. Учитывать только английские буквы.
+'''Посчитать количество строчных (маленьких) и прописных (больших)
+ букв в введенной строке. Учитывать только английские буквы.
 '''
 text_input = input('Kindly input some English text: ')
 count_lowercase = 0
 count_uppercase = 0
-print("The length of the text input is {} symbols". format(len(text_input)))  # this line had been added in order to check that the program works properly
+#  line had been added in order to check that the program works properly
+print("The length of the text input is {} symbols". format(len(text_input)))
 for i in text_input:
-    a = 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-    if i in a and i != a:
+    a = 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
+    b = 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+    if i in a or b and i != a and i != b:
         count_lowercase += 1
     else:
         count_uppercase += 1
-print("The count of the letters in lowercase is {}, the count of the letters in uppercase is {}".format(count_lowercase, count_uppercase))
+print("The count of the letters in lowercase is {}, "
+      "the count of the letters in uppercase is {}".format(count_lowercase, count_uppercase))
