4247f4f31a13ff3e0578fa4ea95db8fec2164a79
lenakotishova <86805388+lenakotishova@users.noreply.github.com>
2021-07-24 16:45:37 +0300

    task1

---
+++ src/homework2/task2.py
@@ -0,0 +1,15 @@
+'''Найти самое длинное слово в введенном предложении. Учтите что в предложении есть знаки препинания.
+Подсказки:
+my_string.split([chars]) возвращает список строк.
+len(list) - количество элементов в списке
+'''
+my_string = input()
+a = my_string.replace('.', '')
+not_useful1 = a.replace('-', '')
+not_useful2 = not_useful1.replace('!', '')
+not_useful3 = not_useful2.replace('?', '')
+b = not_useful3.replace(',', '')
+words = b.split(' ')
+l = list(map(len, b.split()))
+print(b, len(b), l)  # just to make sure that this works properly
+print('Here is the longest word: ', max(words, key=len))
