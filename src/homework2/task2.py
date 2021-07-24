515d44f31ada314d1dcf9586dbecee6dc8cbc1ff
Mila Bendina <mila.bendina@gmail.com>
2021-07-24 16:28:31 +0300

    4 tasks check

---
+++ src/homework2/task2.py
@@ -0,0 +1,18 @@
+'''
+Найти самое длинное слово в введенном предложении. 
+Учтите что в предложении есть знаки препинания.
+Подсказки:
+my_string.split([chars]) возвращает список строк.
+len(list) - количество элементов в списке
+'''
+
+
+sent = input('Введите предложение: ')
+punctuation_marks = '.,-;:?!'
+
+for mark in punctuation_marks:
+  sent = sent.replace(mark, '')
+
+words = sent.split(' ')
+longest_word = max(words, key=len)
+print('Самое длинное слово в предложении —', longest_word.lower())
---
+++ src/homework2/task3.py
@@ -0,0 +1,12 @@
+'''
+Вводится строка. Требуется удалить из нее повторяющиеся символы и все пробелы. 
+Например, если было введено "abc cde def", то должно быть выведено "abcdef".
+'''
+
+
+str_ = input()
+new_str = ''
+for i in str_:
+    if i not in new_str and i != ' ':
+        new_str += i
+print(new_str)
