0c5607f21d17f6dc88dea1a008bb41c856136f8c
lenakotishova <86805388+lenakotishova@users.noreply.github.com>
2021-07-24 23:18:13 +0300

    Revert "task1"
    This reverts commit 87b06802686526eda61da9d9503e2dc766bc230a.
    
    # Conflicts:
    #	src/homework2/task1.py
    
    This was added by mistake, I am doing my best to delete this commit, however, haven't succeeded yet

--- src/homework2/task6.py
+++ src/homework2/task6.py
@@ -8,10 +8,10 @@ digits = int(input("Please, input the number for checking: "))
 a = int(digits)
 reverse = 0
 while digits != 0:
-   last_digit = digits % 10
-   reverse = reverse * 10 + last_digit  # mirroring the input number
-   digits = int(digits / 10)
+    last_digit = digits % 10
+    reverse = reverse * 10 + last_digit  # mirroring the input number
+    digits = int(digits / 10)
 if a == reverse:
-   print("Yes, that's a palindrome")
+    print("Yes, that's a palindrome")
 else:
-   print("No, it's not a palindrome")
+    print("No, it's not a palindrome")
