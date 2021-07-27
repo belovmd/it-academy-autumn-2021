79915f1a11e6a7e33e30a6d20bf344291c10e16d
TatSmat <smatova.tatiana@gmail.com>
2021-07-27 10:42:50 +0300

    correct print template and input line

--- src/homework2/task2.py
+++ src/homework2/task2.py
@@ -3,8 +3,10 @@
 Подсказки:
 my_string.split([chars]) возвращает список строк.
 len(list) - количество элементов в списке
+my_string = 'Главный вопрос Жизни, Вселенной и вообще! The Ultimate Question of Life, the Universe, and Everything!'
 """
-my_string = 'Главный вопрос Жизни, Вселенной и вообще!..'
+my_string = input('Input the string: ')
+template = "The longest word is : {longest_word}"
 punctuation_marks = '.,/?!-:;'
 for mark in punctuation_marks:
     my_string = my_string.replace(mark, '')
@@ -13,4 +15,4 @@ longest_word = ''
 for word in clear_string:
     if len(word) > len(longest_word):
         longest_word = word
-print(longest_word)
+print(template.format(longest_word=longest_word))
