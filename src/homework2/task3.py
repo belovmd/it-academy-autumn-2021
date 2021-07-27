23c7ff677e7e2596276c3d98a76293b40d44d242
TatSmat <smatova.tatiana@gmail.com>
2021-07-27 10:51:36 +0300

    correct print format

--- src/homework2/task3.py
+++ src/homework2/task3.py
@@ -4,9 +4,10 @@
 my_string = input('Input the string: ')
 my_string = my_string.replace(' ', '')
 new_string = my_string[0]
+template = "The string of unique symbols is : {new_string}"
 for i in range(0, len(my_string)):
     if my_string[i] not in new_string:
         new_string += my_string[i]
     else:
         new_string = new_string
-print(new_string)
+print(template.format(new_string=new_string))
