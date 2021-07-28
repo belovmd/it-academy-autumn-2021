bbe5267bc40f155a13b7fe177e663c050fcf5ce5
TatSmat <smatova.tatiana@gmail.com>
2021-07-28 19:23:50 +0300

    deleted trailing whitespace

--- src/homework2/task8.py
+++ src/homework2/task8.py
@@ -1,7 +1,8 @@
 """
 Task_8_1 https://www.hackerrank.com/challenges/capitalize/problem
 Difficulty - Easy
-You are asked to ensure that the first and last names of people begin with a capital letter in their passports. 
+You are asked to ensure that the first and last names of people begin with
+a capital letter in their passports.
 For example, alison heck should be capitalised correctly as Alison Heck.
 Given a full name, your task is to capitalize the name appropriately.
 Input Format:
@@ -9,7 +10,8 @@ A single line of input containing the full name, S.
 Constraints:
 * 0<len(S)<1000
 * The string consists of alphanumeric characters and spaces.
-Note: in a word only the first character is capitalized. Example 12abc when capitalized remains 12abc.
+Note: in a word only the first character is capitalized. Example 12abc when
+capitalized remains 12abc.
 Output Format:
 Print the capitalized string, S.
 """
@@ -44,7 +46,7 @@ else:
 # на сайте проверку не прошла. Хотя код работает.
 
 
-""" 
+"""
 Task_8_2 https://www.hackerrank.com/challenges/python-print/problem
 Difficulty - Easy
 The included code stub will read an integer,n, from STDIN.
@@ -70,7 +72,7 @@ if n < 1 or n > 150:
 
 # создание строки из чисел
 output_list = ''
-for num in range(1, n+1):
+for num in range(1, n + 1):
     output_list = output_list + str(num)
 print(output_list)
 
@@ -83,9 +85,9 @@ print(output_list)
 """
 Task_8_3 https://www.hackerrank.com/contests/it-academy-02/challenges/challenge-1975/submissions
 Difficulty: Medium
-Напишите программу, которая по введённому натуральному числу определит, 
+Напишите программу, которая по введённому натуральному числу определит,
 является ли год с данным номером високосным и выведет количество дней в нём (365 или 366). 
-Подсказка: в соответствии с григорианским календарем, год является високосным, 
+Подсказка: в соответствии с григорианским календарем, год является високосным,
 если его номер кратен 4, но не кратен 100, а также если он кратен 400.
 Input Format:
 Одно натуральное число
@@ -110,8 +112,8 @@ else:
 """
 Task_8_4 https://www.hackerrank.com/challenges/triangle-quest-2/problem
 Difficulty - Medium
-You are given a positive integer .
-Your task is to print a palindromic triangle of size .
+You are given a positive integer.
+Your task is to print a palindromic triangle of size.
 For example, a palindromic triangle of size  is:
 1
 121
@@ -121,8 +123,8 @@ For example, a palindromic triangle of size  is:
 You can't take more than two lines. The first line (a for-statement) is already written for you.
 You have to complete the code using exactly one print statement.
 Note:
-Using anything related to strings will give a score of .
-Using more than one for-statement will give a score of .
+Using anything related to strings will give a score of 0.
+Using more than one for-statement will give a score of 0.
 Input Format:
 A single line of input containing the integer n.
 Constraints:
@@ -155,8 +157,8 @@ for number in range(1, n + 1):
 
 # после долгих изучений свойств функции print, найдено решение из 2-х строк (по условию задачи)
 # тест на сайте не прошел, хотя условия соблюдены и код работает
-for num in range(1, int(input())+1):
-    print(*[dig for dig in range(1, num + 1)], *[dig for dig in range(num-1, 0, -1)],  sep='')
+for num in range(1, int(input()) + 1):
+    print(*[dig for dig in range(1, num + 1)], *[dig for dig in range(num - 1, 0, -1)], sep='')
 
 
 """
@@ -165,9 +167,9 @@ Difficulty - Medium
 В системе координат на плоскости выделяют четверти:
 I-й четветри соответствуют точки, имеющие обе (x и y) положительные координаты
 II-ая четверть: x < 0, y > 0
-III-ая четверть: x < 0, y < 0.
+III-ая четверть: x < 0, y < 0
 IV-ая четверть: x > 0, y < 0
-Напишите программу, которая по заданным X и Y (целые числа, которые вводятся пользователем), 
+Напишите программу, которая по заданным X и Y (целые числа, которые вводятся пользователем),
 определяет координатную четверть и выводит "I", "II", "III" или "IV"
 
 Input Format:
