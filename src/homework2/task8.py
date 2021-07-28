d4b91e2993f1743366af14c8942754faa2d12d93
EvgenyLav <evgenlav1993@gmail.com>
2021-07-28 19:17:49 +0300

    task8done

--- src/homework2/task8.py
+++ src/homework2/task8.py
@@ -3,4 +3,4 @@ You are given a string and you have to find its first word.
 """
 str_ = "Hello World"
 word = str_.split()  # делим строку на слова
-print(word[0])  # выводим первое слово
+print(word[0])       # выводим первое слово
