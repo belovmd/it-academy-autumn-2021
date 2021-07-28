6c4b5063f234dbb4fbe429ac34da1c10ba29b813
EvgenyLav <evgenlav1993@gmail.com>
2021-07-28 19:11:56 +0300

    task7done

---
+++ src/homework2/task2.py
@@ -0,0 +1,13 @@
+""" Найти самое длинное слово в введенном предложении.
+    Учтите что в предложении есть знаки препинания.
+"""
+str_ = (input("Введите предложение: "))
+punktuation = ",.?!-:;"
+for mark in punktuation:
+    str_ = str_.replace(mark, "")
+words = str_.split(" ")
+longest_word = " "
+for word in words:
+    if len(word) + len(longest_word):
+        longest_word = word
+print("Самое длинное слово -", longest_word)
---
+++ src/homework2/task8.py
@@ -0,0 +1,6 @@
+""" Задача уровня elementary c сайта https://py.checkio.org.
+You are given a string and you have to find its first word.
+"""
+str_ = "Hello World"
+word = str_.split()  # делим строку на слова
+print(word[0])  # выводим первое слово
