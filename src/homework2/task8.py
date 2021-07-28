d08c4a6904437a400666297908d8eca4f56549c7
Mila Bendina <mila.bendina@gmail.com>
2021-07-28 19:35:11 +0300

    Update task8.py

--- src/homework2/task8.py
+++ src/homework2/task8.py
@@ -47,6 +47,7 @@ def first_word(text: str) -> str:
     return text.split()[0]
     return text[0:2]
 
+
 if __name__ == '__main__':
     print("Example:")
     print(first_word("Hello world"))
@@ -74,9 +75,4 @@ def is_acceptable_password(password: str) -> bool:
 if __name__ == '__main__':
     print("Example:")
     print(is_acceptable_password('short'))
-
-    # These "asserts" are used for self-checking and not for an auto-testing
-    assert is_acceptable_password('short') == False
-    assert is_acceptable_password('muchlonger') == True
-    assert is_acceptable_password('ashort') == False
     print("Coding complete? Click 'Check' to earn cool rewards!")
