eae652362ae2914fbdd275c3e8fd31fbbd7412c1
Vinichek <Vinichek_Yana@mail.ru>
2021-07-28 18:56:14 +0300

    Homework2: Yana Kaziychyts

---
+++ src/homework2/task2.py
@@ -0,0 +1,17 @@
+'''Найти самое длинное слово в введенном предложении. Учтите что в предложении есть знаки препинания.
+Подсказки:
+my_string.split([chars]) возвращает список строк.
+len(list) - количество элементов в списке
+'''
+
+print('Введите предложение')
+my_string = input()
+long = ''
+punkt_marks = '.,!?;:-'
+for _ in punkt_marks:
+  my_string = my_string.replace(_,'')
+my_string = my_string.split(' ')
+for list in my_string:
+  if len(list) > len(long):
+    long = list
+print(long)
---
+++ src/homework2/task8.py
@@ -0,0 +1,81 @@
+'''Given an integer, n, perform the following conditional actions:
+If n is odd, print Weird
+If n is even and in the inclusive range of 2 to 5, print Not Weird
+If n is even and in the inclusive range of 6 to 20, print Weird
+If n is even and greater than 20, print Not Weird
+Input Format
+A single line containing a positive integer, n.
+Constraints
+1 <= n <= 100
+Output Format
+Print Weird if the number is weird. Otherwise, print Not Weird.
+'''
+
+print('Enter a positive integer')
+n = int(input())
+if (n % 2) or ((not n % 2) and (6 <= n <= 20)):
+  print('Weird')
+elif ((not n % 2) and (2 <= n <= 5) or (20 < n <= 100)):
+  print('No weird')
+
+
+'''The provided code stub reads and integer, n, from STDIN. For all non-negative integers i < n, print i^2.
+Example 
+n = 3
+The list of non-negative integers that are less than n = 3 is [0, 1, 2]. 
+Print the square of each number on a separate line.
+0
+1
+4
+Input Format
+The first and only line contains the integer, n.
+Constraints
+1 <= n <= 20
+Output Format
+Print n lines, one corresponding to each i.
+'''
+
+print('Enter an integer')
+n = int(input())
+if (n >= 1) and (n <= 20):
+  for i in range (0, n):
+    print(i ** 2)
+
+
+'''В системе координат на плоскости выделяют четверти:
+I-й четветри соответствуют точки, имеющие обе (x и y) положительные координаты
+II-ая четверть: x < 0, y > 0
+III-ая четверть: x < 0, y < 0
+IV-ая четверть: x > 0, y < 0
+Напишите программу, которая по заданным X и Y (целые числа, которые вводятся пользователем),
+определяет координатную четверть и выводит "I", "II", "III" или "IV".
+'''
+
+print('Введите координату x')
+x = int(input())
+print('Введите координату y')
+y = int(input())
+if (x > 0) and (y > 0):
+  print('Точка с такими координатами находится в I четверти')
+elif (x < 0) and (y > 0):
+  print('Точка с такими координатами находится в II четверти')
+elif (x < 0) and (y < 0):
+  print('Точка с такими координатами находится в III четверти')
+else:
+  print ('Точка с такими координатами находится в IV четверти')
+
+
+'''Напишите программу, которая по введённому натуральному числу определит,
+является ли год с данным номером високосным и выведет количество дней в нём (365 или 366).
+Подсказка: в соответствии с григорианским календарем, год является високосным,
+если его номер кратен 4, но не кратен 100, а также если он кратен 400.
+'''
+
+print('Введите год, о котором Вы хотите узнать високосный ли он')
+year = int(input())
+if (year % 400 == 0) or ((year % 4 == 0) and (year % 100 != 0)):
+  print('Год високосный')
+else:
+  print ('Год не високосный')
+
+
