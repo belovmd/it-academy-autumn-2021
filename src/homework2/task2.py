002473a10e4922b84fee827d8db8e1c744451671
Igorsuper <igor03111993@mail.ru>
2021-07-29 23:08:49 +0300

    task3

--- src/homework2/task2.py
+++ src/homework2/task2.py
@@ -1,11 +1,11 @@
 '''Найти самое длинное слово в введенном предложении. Учтите что в предложении есть знаки препинания
-Подсказки
+Подсказки:
 my_string.split([chars]) возвращает список строк.
 len(list) - количество элементов в списке
 '''
-string = input (write sentences)
-longest_word =  
-for word in string.split()
-  if len(word)  len(longest_word)
+string = input ("write sentences")
+longest_word = " "
+for word in string.split():
+  if len(word) > len(longest_word):
     longest_word = word
 print(longest_word)
