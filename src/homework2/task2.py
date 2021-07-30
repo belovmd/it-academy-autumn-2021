205f8e975ab2c572b1395b69727cb3ba3ab082dc
kirerof <kirill.erofeev.07@gmail.com>
2021-07-31 00:30:50 +0300

    1 commit

---
+++ src/homework2/task2.py
@@ -0,0 +1,16 @@
+"""Найти самое длинное слово в введенном предложении.
+Учтите что в предложении есть знаки препинания."""
+my_str = input('Введите строку: ')
+punctuation = '.,!?:;'
+
+for mark in punctuation:
+    my_str = my_str.replace(mark, '')
+
+words = my_str.split(' ')
+longest_word = ''
+
+for word in words:
+    if len(word) > len(longest_word):
+        longest_word = word
+
+print(longest_word)
