0f191371bf2283256473353377f1301aa9e308ec
lenakotishova <86805388+lenakotishova@users.noreply.github.com>
2021-07-31 15:16:13 +0300

    the final

--- Homework1 Lena Kotishova.py
+++ Homework1 Lena Kotishova.py
@@ -35,13 +35,14 @@ while babies < 100:
     print('This generation has {0} babies'.format(babies))
     parents, babies = (babies, parents + babies)
 
+
 # 5 lines: Functions
 
 
 def greet(name):
-
-
     print('Hello', name)
+
+
 greet('Jack')
 greet('Jill')
 greet('Bob')
