3ec13aebb5f83583802d013cf9dd6673bb62b210
AlenaCharniak <86879691+AlenaCharniak@users.noreply.github.com>
2021-08-02 01:41:48 +0300

    Update task2.py

--- src/homework2/task2.py
+++ src/homework2/task2.py
@@ -6,8 +6,6 @@ predl = input('Введите предложение: ')
 i = predl.split(' ')
 predl = []
 for element in i:
-   element = element.strip('.,:;!?)')
-   predl.append(element)
-print('Самое длинное слово: ', max(predl,key=len))
-
-
+    element = element.strip('.,:;!?)')
+    predl.append(element)
+print('Самое длинное слово: ', max(predl, key=len))
