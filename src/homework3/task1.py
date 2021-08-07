1a74f60ac59f250b131db054d8496115b6e2a76f
Dmitry <froz3n90@gmail.com>
2021-08-07 19:56:29 +0300

    hw3 try1

---
+++ src/homework3/task1.py
@@ -0,0 +1,17 @@
+"""
+FizzBuzz
+Напишите программу, которая печатает цифры от 1 до 100,
+но вместо чисел, кратных 3 пишет Fizz, вместо чисел кратный 5
+пишет Buzz, а вместо чисел одновременно кратных и 3 и 5 - FizzBuzz
+"""
+
+for num in range(1,101):
+    if not num % 3 and not num % 5:
+        print('FizzBuzz')
+    elif not num % 5:
+        print('Buzz')
+    elif not num % 3:
+        print('Fizz')
+    else:
+        print(num)
+
