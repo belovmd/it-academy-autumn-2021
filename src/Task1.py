4447b5375d5edd5fcf46e12cba26e1405aaacc3f
RamanSabeshkin <sabeshrom@gmail.com>
2021-08-08 20:54:02 +0200

    test

--- src/Task1.py
+++ src/Task1.py
@@ -1,15 +1,22 @@
-def generators():
-    list_ = [i + k for i in 'abc' for k in 'abc']
-    list_2 = list_[1::2]
-    list_3 = [str(i) + 'a' for i in range(1, 5)]
-    print(f"list_ => {list_}")
-    print(f"list_2 => {list_2}")
-    el = list_3.pop(1)
-    print(f"list_3 => {list_3} => element {el} popped")
-    list_4 = list_3[:]
-    list_4.insert(1, el)
-    print(f"list_4 => {list_4} <= element {el} inserted")
+"""FizzBuzz
+Напишите программу, которая печатает цифры от 1 до 100, но вместо чисел, кратных 3 пишет Fizz,
+вместо чисел кратный 5 пишет Buzz, а вместо чисел одновременно кратных и 3 и 5 - FizzBuzz
+"""
+
+
+def crazy_function():
+    list_ = []
+    for i in range(1, 101):
+        if not i % 3:
+            list_.append('Fizz')
+        elif not i % 5:
+            list_.append('Buzz')
+        elif not i % 3 and i % 5:
+            list_.append('FizzBuzz')
+        else:
+            list_.append(i)
+    return list_
 
 
 if __name__ == '__main__':
-    generators()
+    print(crazy_function())
---
+++ src/Task5.py
@@ -0,0 +1,22 @@
+"""Уникальные элементы в списке
+Дан список. Выведите те его элементы, которые встречаются в списке только один раз.
+Элементы нужно выводить в том порядке, в котором они встречаются в списке.
+"""
+
+
+def find_single():
+    string_ = "1 2 4 5 7 7 4 5 3 2 1 2 2 1 1 10 10 10 11 36 36 37 a a g g g n f j k l"
+    list_ = string_.split()
+    dict_ = dict.fromkeys(list_, 0)
+    l_ = len(list_)
+    i = 1
+    for k in list_:
+        for j in list_[i:l_]:
+            if k == j:
+                dict_[k] += 1
+        i += 1
+    print("Уникальные элементы в строке:\n" + " ".join("{}".format(k) for k, v in dict_.items() if not v))
+
+
+if __name__ == '__main__':
+    find_single()
