37ff3310330963c41839e499402f8621cf1c2426
lenakotishova <86805388+lenakotishova@users.noreply.github.com>
2021-08-09 13:50:20 +0300

    re-update the solution

--- src/homework2/task2.py
+++ src/homework2/task2.py
@@ -4,13 +4,13 @@ my_string.split([chars]) возвращает список строк.
 len(list) - количество элементов в списке
 '''
 
-my_string = input('Kindly input some text: ')
-a = my_string.replace('.', '')
-not_useful1 = a.replace('-', '')
-not_useful2 = not_useful1.replace('!', '')
-not_useful3 = not_useful2.replace('?', '')
-b = not_useful3.replace(',', '')
-words = b.split(' ')
-separate = list(map(len, b.split()))
-print(b, len(b), separate)  # just to make sure that this works properly
-print('Here is the longest word: ', max(words, key=len))
+string = input('Kindly input some text: ')
+punctuation = '.,?-:;!'
+for mark in punctuation:
+    string = string.replace(mark, '')
+words = string.split(' ')
+longest_word = ''
+for word in words:
+    if len(word) > len(longest_word):
+        longest_word = word
+print('The longest word is ', longest_word)
