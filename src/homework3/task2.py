c852a184b5118d1879904c173d4a45a40f77633f
lenakotishova <86805388+lenakotishova@users.noreply.github.com>
2021-08-12 22:51:39 +0300

    Update task2.py

--- src/homework3/task2.py
+++ src/homework3/task2.py
@@ -3,8 +3,8 @@
 alphabet = ['a', 'b', 'c', 'd']
 i = [alphabet]
 lst1 = [element1+element2 for element1 in alphabet for element2
-        in alphabet if 'a' <= element1 <= 'b'
-        and 'a' < element2 <= 'd']
+        in alphabet if 'a' <= element1 <= 'b' and 
+        'a' < element2 <= 'd']
 print(lst1)
 
 # Используйте на предыдущий список slice чтобы получить следующий:
