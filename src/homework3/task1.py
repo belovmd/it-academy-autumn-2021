adecb5089c30e00ec3196c21c0fe71d1f08a5889
Igorsuper <igor03111993@mail.ru>
2021-08-16 20:10:49 +0300

    123

--- src/homework3/task1.py
+++ src/homework3/task1.py
@@ -1,15 +1,15 @@
 '''FizzBuzz
 Напишите программу, которая печатает цифры от 1 до 100, но вместо чисел,
 кратных 3 пишет Fizz, вместо чисел кратный 5 пишет Buzz,
-а вместо чисел одновременно кратных и 3 и 5 - FizzBuzz'''
+а вместо чисел одновременно кратных 3 и 5 - FizzBuzz'''
 
 list = []
 for i in range(1, 101):
-    if (i % 15) == 0:
+    if not (i % 15):
         list.append("FizzBuzz")
-    elif i % 3 == 0:
+    elif not i % 3:
         list.append('Fizz')
-    elif i % 5 == 0:
+    elif not i % 5:
         list.append('Buzz')
     else:
         list.append(i)
