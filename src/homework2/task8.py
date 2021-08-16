26362c2b4cbef7562dfc0dd546029b776418797c
Anna Safyan <01forstudy@gmail.com>
2021-08-16 15:20:15 +0300

    fix checks of flake8

--- src/homework2/task8.py
+++ src/homework2/task8.py
@@ -5,6 +5,7 @@ return the middle 2 characters.
 https://www.codewars.com/kata/56747fd5cb988479af000028/train/python
 """
 
+
 def get_middle(s):
     if len(s) % 2:
         return s[len(s) // 2]
@@ -14,14 +15,13 @@ def get_middle(s):
 
 print(get_middle(input('Введите слово: ')))
 
-
-
 """
 If we list all the natural numbers below 10 that are multiples of 3 or 5,
 we get 3, 5, 6 and 9. The sum of these multiples is 23. Finish the solution
 so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
 https://www.codewars.com/kata/514b92a657cdc65150000006/train/python
 """
+
 
 def solution(number):
     out_number = 0
@@ -33,13 +33,12 @@ def solution(number):
 
 print(solution(int(input('Введите число: '))))
 
-
-
 """ 
 In this little assignment you are given a string of space separated numbers,
 and have to return the highest and lowest number.
 https://www.codewars.com/kata/554b4ac871d6813a03000035/train/python
 """
+
 
 def high_and_low(numbers):
     numbers = numbers.split(' ')
@@ -51,13 +50,12 @@ def high_and_low(numbers):
 
 print(high_and_low(input('Введите числа: ')))
 
-
-
 """ 
 Write an algorithm that takes an array and moves all of the zeros to the end,
 preserving the order of the other elements.
 https://www.codewars.com/kata/52597aa56021e91c93000cb0/train/python
 """
+
 
 def move_zeros(array):
     for element in ['(', ',', ')', '[', ']', '{', '}']:
@@ -75,14 +73,13 @@ def move_zeros(array):
 
 print(move_zeros(input('Введите множество: ')))
 
-
-
 """ 
 Write a function that takes a string of parentheses, and determines if the order
 of the parentheses is valid. The function should return true if the string is valid,
 and false if it's invalid.
 https://www.codewars.com/kata/52774a314c2333f0a7000688/train/python
 """
+
 
 def valid_parentheses(str_):
     for element in str_:
