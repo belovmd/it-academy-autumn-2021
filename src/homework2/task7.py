93028703b2fc87ddee15867d2ad3ce920940b03c
KirylErofeev <kirill.erofeev.07@gmail.com>
2021-08-17 15:53:14 +0300

    fix test-flake8

--- src/homework2/task7.py
+++ src/homework2/task7.py
@@ -7,8 +7,8 @@ c = int(input('сторона c:'))
 
 if a + b > c and a + c > b and b + c > a:
     semi_perimeter = (a + b + c) / 2
-    Square = ((semi_perimeter * (semi_perimeter - a) * (semi_perimeter - b) *
-              (semi_perimeter - c)) ** 0.5)
+    Square = ((semi_perimeter * (semi_perimeter - a) * (semi_perimeter - b)
+               * (semi_perimeter - c)) ** 0.5)
     print('Это треугольник, с площадью ', Square)
 else:
     print('Это не треугольник')
