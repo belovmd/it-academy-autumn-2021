f9bdb325079ca33ca347b47a83e1eb580f1edeba
raivania <le.pzzbe@gmail.com>
2021-08-21 14:02:54 +0300

    task8

--- src/homework2/task8.py
+++ src/homework2/task8.py
@@ -4,12 +4,12 @@ cannot contain anything but exactly 4 digits or exactly 6 digits.
 
 If the function is passed a valid PIN string, return true, else return false.
 """
-# password = 1257
-# attempt = int(input('Enter your password: '))
-# if password == attempt:
-#     print('true')
-# else:
-#     print('false')
+password = 1257
+attempt = int(input('Enter your password: '))
+if password == attempt:
+    print('true')
+else:
+    print('false')
 
 
 """
@@ -18,15 +18,15 @@ If the word's length is odd, return the middle character. If the word's length i
 return the middle 2 characters.
 """
 
-# word = input('Enter your word: ')
-# letters = len(word)
-# if len(word) % 2:
-#    middle = (letters // 2)
-#    print(word[middle])
-# else:
-#    middle1 = ((letters // 2) - 1)
-#    middle2 = (letters // 2)
-#    print(word[middle1] + word[middle2])
+word = input('Enter your word: ')
+letters = len(word)
+if len(word) % 2:
+   middle = (letters // 2)
+   print(word[middle])
+else:
+   middle1 = ((letters // 2) - 1)
+   middle2 = (letters // 2)
+   print(word[middle1] + word[middle2])
 
 
 """
@@ -34,29 +34,29 @@ You will be given an array of numbers. You have to sort the odd numbers in ascen
 while leaving the even numbers at their original positions.
 """
 
-# str_ = input('Input array: ')
-# str_ = str_.strip()
-# str_ = str_.split(' ')
-# list_ = []
-#
-# for char in str_:
-#     list_.append(int(char))
-#
-# list_odd = []
-# list_even = []
-# list_new = []
-# for i in list_:
-#     if i % 2:
-#         list_odd.append(i)
-#     else:
-#         list_even.append(i)
-# list_odd.sort()
-# for a in range(max(len(list_odd), len(list_even))):
-#     if a < len(list_odd):
-#         list_new.append(list_odd[a])
-#     if a < len(list_even):
-#         list_new.append(list_even[a])
-# print(list_, list_new)
+str_ = input('Input array: ')
+str_ = str_.strip()
+str_ = str_.split(' ')
+list_ = []
+
+for char in str_:
+    list_.append(int(char))
+
+list_odd = []
+list_even = []
+list_new = []
+for i in list_:
+    if i % 2:
+        list_odd.append(i)
+    else:
+        list_even.append(i)
+list_odd.sort()
+for a in range(max(len(list_odd), len(list_even))):
+    if a < len(list_odd):
+        list_new.append(list_odd[a])
+    if a < len(list_even):
+        list_new.append(list_even[a])
+print(list_, list_new)
 
 
 """
@@ -75,5 +75,11 @@ array_ = []
 for char in str_:
     array_.append(int(char))
 array_.sort()
-sum_ = array_[0] + array_[1]
-print(sum_)
+print(array_[0] + array_[1])
+
+"""
+Given an array of integers, find the one that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times
+"""
+
