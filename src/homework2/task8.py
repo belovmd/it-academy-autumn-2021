3fc30ffbe1a4074d088dd06c2f3ed2aea9aa4e0a
EvgenyLav <evgenlav1993@gmail.com>
2021-08-26 23:32:59 +0300

    fix

--- src/homework2/task8.py
+++ src/homework2/task8.py
@@ -23,7 +23,8 @@ chislo = int(input())
 chislo1 = str(chislo)
 print("Колличество цифр числа", chislo, '-', len(chislo1))
 
-""" Задача easy с сайта  www.hackerrank.com. Given an integer, n, perform the following conditional actions:
+""" Задача easy с сайта  www.hackerrank.com. Given an integer, 
+n, perform the following conditional actions:
 If n is odd, print Weird
 If n is even and in the inclusive range of 2 to 5, print Not Weird
 If n is even and in the inclusive range of 6 to 20, print Weird
