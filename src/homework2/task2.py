6a5e3a538bd405a7868db31ff2560d28bdd29b7a
KACHKAYEUYAUHENI <83453537+KACHKAYEUYAUHENI@users.noreply.github.com>
2021-08-26 21:33:34 +0300

    Add files via upload

--- src/homework2/task2.py
+++ src/homework2/task2.py
@@ -1,8 +1,8 @@
 # Найти самое длинное слово в введенном предложении. Учтите что в предложении есть знаки препинания.
 
 string_ = (input("Ввведите строку:"))
-marks = ".,?-_:;!()/ "
-for i in marks:
-    string_ = string_.replace(i, " ")
+for i in string_:
+    if not i.isalnum():
+       string_ = string_.replace(i, " ")
 words = string_.split()
 print(max(words, key=len))
