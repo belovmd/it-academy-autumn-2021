6012ec38369e8e8717aa708018ed364f93be0592
lenakotishova <86805388+lenakotishova@users.noreply.github.com>
2021-08-31 01:40:56 +0300

    Update task7.py

--- src/homework2/task7.py
+++ src/homework2/task7.py
@@ -3,16 +3,14 @@
 # Если стороны определяют треугольник, найти его площадь.
 # Если нет, вывести сообщение о неверных данных.
 
-print('Please enter the triangle sides in cm below: ')
-a = int(input('First side: '))
-b = int(input('Second one: '))
-c = int(input('Third one: '))
-triangle1 = a + c
-triangle2 = a + b
-half_perimiter = int((a + b + c) / 2)  
-S = int(P * (P - a) * (P - b) * (P - c))
-square = S ** 0.5
-if (a + b > c) and (a + c > b) and (b + c > a):
-    print('Square is ', square)
+print('Please, enter the triangle sides in cm below: ')
+side1 = int(input('First side: '))
+side2 = int(input('Second one: '))
+side3 = int(input('Third one: '))
+if (side1 + side2 > side3) and (side1 + side3 > side2) and (side2 + side3 > side1):
+    half_perimiter = float((side1 + side2 + side3) / 2)
+    triange_square = float(((half_perimiter * (half_perimiter - side1) * (half_perimiter - side2) * (half_perimiter - side3))) ** 0.5)
+    print('Square is ', triange_square, 'square cm')
 else:
     print('The triangle is not possible')
+    
