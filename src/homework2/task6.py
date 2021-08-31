5d34c2ccd9f05367880aa24a8171f4dfd159d1c8
lenakotishova <86805388+lenakotishova@users.noreply.github.com>
2021-08-31 03:19:13 +0300

    Update task6.py

--- src/homework2/task6.py
+++ src/homework2/task6.py
@@ -9,7 +9,7 @@ a = int(digits)
 reverse_order = 0
 while digits != 0:
     last_digit = digits % 10
-    reverse_order = reverse_order * 10 + last_digit  
+    reverse_order = reverse_order * 10 + last_digit
     digits = int(digits // 10)
 if a == reverse_order:
     print("Yes, that's a palindrome")
