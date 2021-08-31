cd561e117dfea13596ae6d12d2b7d7df634c3d8a
lenakotishova <86805388+lenakotishova@users.noreply.github.com>
2021-08-31 03:20:44 +0300

    Update task7.py

--- src/homework2/task7.py
+++ src/homework2/task7.py
@@ -14,4 +14,3 @@ if (side1 + side2 > side3) and (side1 + side3 > side2) and (side2 + side3 > side
     print('Square is ', triange_square, 'square cm')
 else:
     print('The triangle is not possible')
-    
