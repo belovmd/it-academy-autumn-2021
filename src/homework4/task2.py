fb7385dded3df9d66baca1f0da42ddac2321da8a
raivania <le.pzzbe@gmail.com>
2021-08-31 12:48:05 +0300

    a

--- src/homework4/task2.py
+++ src/homework4/task2.py
@@ -27,14 +27,46 @@ Ukraine
 Russia
 Russia
 """
-import re
-# str_ = 'a       b b c'
-# lst_ = []
-# str_ = str_.replace(' ', '')
-# for i in str_:
-#     lst_.append(i)
-# print(lst_)
 
-str_ = 'aaa5 55      b b c \n'
-lst_ = re.findall(r'\S+', str_)
-print(lst_, len(lst_))
+dct_ = {}
+country_amount = int(input('Input amount of countries: '))
+while country_amount > 0:
+    country = input('Input country with cities: ')
+    lst_country = country.split(' ')
+    dct_loop = dict([(lst_country[0], lst_country[1:])])
+    dct_.update(dct_loop)
+    country_amount -= 1
+print(dct_.keys())
+
+city_amount = int(input('Input amount of requests: '))
+while city_amount > 0:
+    city = input('Input required city: ')
+    for key, value in dct_.items():
+        if city == value:
+            print(key)
+        city_amount -= 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+# rus = input('Input country with cities: ')
+# lst_rus = rus.split(' ')
+# fra = input('Input country with cities: ')
+# lst_fra = fra.split(' ')
+# ukr = input('Input country with cities: ')
+# lst_ukr = ukr.split(' ')
+# bel = input('Input country with cities: ')
+# lst_bel = bel.split(' ')
+# dct_ = dict(([(lst_rus[0], lst_rus[1:]), (lst_fra[0], lst_fra[1:3]),
+#             (lst_ukr[0], lst_ukr[1:4]), (lst_bel[0], lst_bel[1:3])]))
+# print(dct_)
+
