d3abd6ed99bdba8c7374704546bb518efb27039c
lenakotishova <86805388+lenakotishova@users.noreply.github.com>
2021-08-31 03:18:50 +0300

    Update task2.py

--- src/homework2/task2.py
+++ src/homework2/task2.py
@@ -1,12 +1,12 @@
-# Найти самое длинное слово в введенном предложении. Учтите что в предложении есть знаки препинания 
-# Подсказки: 
-# my_string.split([chars]) возвращает список строк. 
-# len(list) - количество элементов в списке 
-import string 
-result = string.punctuation 
-sentence = str(input()) 
-for letter in sentence: 
-    if letter in result: 
-       sentence = sentence.replace(letter, '') 
-sentence = sentence.split() 
+# Найти самое длинное слово в введенном предложении. Учтите что в предложении есть знаки препинания
+# Подсказки:
+# my_string.split([chars]) возвращает список строк.
+# len(list) - количество элементов в списке
+import string
+result = string.punctuation
+sentence = str(input())
+for letter in sentence:
+    if letter in result:
+        sentence = sentence.replace(letter, '')
+sentence = sentence.split()
 print('The longest word in the sentence is: ', max(sentence, key=len))
