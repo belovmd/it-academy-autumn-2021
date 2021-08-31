68beecaee18a2264bf7444c312687c7e51037985
raivania <le.pzzbe@gmail.com>
2021-08-31 15:43:04 +0300

    hw

---
+++ src/homework4/task7.py
@@ -0,0 +1,13 @@
+"""
+Даны два натуральных числа. Вычислите их наибольший общий делитель
+при помощи алгоритма Евклида (мы не знаем функции и рекурсию).
+"""
+
+num1 = int(input('Input first number: '))
+num2 = int(input('Input second number: '))
+while num1 and num2:
+    if num1 > num2:
+        num1 = num1 % num2
+    else:
+        num2 = num2 % num1
+print(num1, num2)
