e820e93906469d8970af5eb7d15105b307f89f71
raivania <le.pzzbe@gmail.com>
2021-09-06 21:53:09 +0300

    task 2

--- src/homework4/task2.py
+++ src/homework4/task2.py
@@ -28,31 +28,18 @@ Russia
 Russia
 """
 
-# dct_ = {}
-# country_amount = int(input('Input amount of countries: '))
-# for i in range(country_amount):
-#     country = input('Input country with cities: ')
-#     lst_country = country.split(' ')
-#     dct_loop = dict([(lst_country[0], lst_country[1:])])
-#     dct_.update(dct_loop)
-#
-# city_amount = int(input('Input amount of requests: '))
-# for i in range(country_amount):
-#     city = input('Input required city: ')
-#     for key, value in dct_.items():
-#         for el in value:
-#             if city == el:
-#                 print(key)
+dct_ = {}
+country_amount = int(input('Input amount of countries: '))
+for i in range(country_amount):
+    country = input('Input country with cities: ')
+    lst_country = country.split(' ')
+    dct_loop = dict([(lst_country[0], lst_country[1:])])
+    dct_.update(dct_loop)
 
-motherland = {}
-lst_ = []
-for i in range(int(input('Введите колличество стран: '))):
-    country, *cities = input('Введите страну и города: ').split()
-    for city in cities:
-        if not motherland[city]:
-            motherland[city] = []
-        motherland[city].append(country)
-for i in range(int(input('Введите число запросов '))):
-    a = motherland[input('Введите город ')]
-    for country in a:
-        print(country)
+city_amount = int(input('Input amount of requests: '))
+for i in range(country_amount):
+    city = input('Input required city: ')
+    for key, value in dct_.items():
+        for el in value:
+            if city == el:
+                print(key)
