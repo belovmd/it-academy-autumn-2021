b6948f1513d79788f0fcfa33029b6e120c501571
TatSmat <smatova.tatiana@gmail.com>
2021-09-08 17:10:55 +0300

    correct range in for-loop. Deleted redundant code in PR

--- src/homework2/task3.py
+++ src/homework2/task3.py
@@ -1,11 +1,10 @@
 """Вводится строка. Требуется удалить из нее повторяющиеся символы и все пробелы.
 Например, если было введено "abc cde def", то должно быть выведено "abcdef"."""
-# my_string = 'The Ultimate Question of Life, the Universe, and Everything!..'
 my_string = input('Input the string: ')
 my_string = my_string.replace(' ', '')
 new_string = my_string[0]
 template = "The string of unique symbols is : {new_string}"
-for i in range(0, len(my_string)):
+for i in range(len(my_string)):
     if my_string[i] not in new_string:
         new_string += my_string[i]
     else:
