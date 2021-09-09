4e8ca03a53e084ccde6af7e6d9a33ca9569fbc27
TatSmat <smatova.tatiana@gmail.com>
2021-09-09 21:39:03 +0300

    changed name and used the recommended a, b = b, a + b

--- src/homework2/task5.py
+++ src/homework2/task5.py
@@ -3,18 +3,16 @@
 # F(n) = F(n-1)+F(n-2)
 
 n = int(input('Введите число n: '))
-template = "Число Фибоначчи = {Fib}"
+template = "Число Фибоначчи = {fib}"
+a = 0
+b = 1
 if n < 0:
     print('Negative')
 elif n <= 1:
-    Fib_n = n
-    print(template.format(Fib=Fib_n))
+    b = n
+    print(template.format(fib=b))
 else:
-    Fib_n_2 = 0
-    Fib_n_1 = 1
-    Fib_n = Fib_n_2 + Fib_n_1
-    for i in range(0, n - 2):
-        Fib_n_2 = Fib_n_1
-        Fib_n_1 = Fib_n
-        Fib_n = Fib_n_2 + Fib_n_1
-    print(template.format(Fib=Fib_n))
+    a, b = b, a + b
+    for i in range(n - 2):
+        a, b = b, a + b
+    print(template.format(fib=b))
