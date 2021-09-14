8b017a04079d9e457bc2bd133690288c31099daa
TatSmat <smatova.tatiana@gmail.com>
2021-09-14 22:55:28 +0300

    1-st

--- src/homework4/task3.py
+++ src/homework4/task3.py
@@ -4,8 +4,5 @@ list_1 = [8, 1, 2, 5, 5, 3]
 list_2 = [8, 8, 8, 3, 4, 1, 6]
 set_1, set_2 = set(list_1), set(list_2)
 new_set = set_1 | set_2
-unique_num = 0
-for i in new_set:
-    unique_num += 1
 print(new_set)
-print(unique_num)
+print(len(new_set), 'различных чисел в двух списках')
