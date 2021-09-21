540398e670323dec44e1f9d4cfb102842e06b223
lenakotishova <86805388+lenakotishova@users.noreply.github.com>
2021-09-21 21:56:22 +0300

    Update task5.py

--- src/homework3/task5.py
+++ src/homework3/task5.py
@@ -1,9 +1,12 @@
 # Дан список. Выведите те его элементы, которые встречаются в списке только один раз.
 # Элементы нужно выводить в том порядке, в котором они встречаются в списке.
 
-lst = [int(x) for x in input('List: ').split()]
-unique_list = []
-for el in lst:
-    if el not in unique_list:
-        unique_list.append(el)
-print('Unique list: ', unique_list)
+def pairs(lst):
+    print('The list: ', lst)
+    print('Unique elements: ')
+    for el in lst:
+        if lst.count(el) == 1:
+            print(el)
+
+lst = [21, 25, 84, 65, 23, 24, 12, 21, 84]
+pairs(lst)
