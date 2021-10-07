572642a44284495c8e4f0428819cb293ebf0f030
TatSmat <smatova.tatiana@gmail.com>
2021-10-07 13:26:07 +0300

    Task 4 Changed set.union to set.intersection

--- src/homework4/task3.py
+++ src/homework4/task3.py
@@ -3,6 +3,5 @@
 list_1 = [8, 1, 2, 5, 5, 3]
 list_2 = [8, 8, 8, 3, 4, 1, 6]
 set_1, set_2 = set(list_1), set(list_2)
-new_set = set_1 | set_2
-print(new_set)
-print(len(new_set), 'различных чисел в двух списках')
+new_set = set_1 & set_2
+print(len(new_set), 'различных чисел одновременно в двух списках')
