006571f284c78bb14f44d0e00a95b7be3dc6af3e
Tatiana Smatova <smatova.tatiana@gmail.com>
2021-10-31 15:36:12 +0300

    HomeWork 5: Task 5 nearest_power_of_two

--- src/homework5/task5.py
+++ src/homework5/task5.py
@@ -1,2 +1,18 @@
 # Написать программу которая находит ближайшую степень двойки к введенному числу.
 # 10(8), 20(16), 1(1), 13(16)
+
+def nearest_power_of_two(n):
+    i = 1
+    while n >= 2**i:
+        i += 1
+    if abs(n-(2**(i-1))) <= abs(n - 2 ** i):
+        print(2**(i-1))
+    else:
+        print(2**i)
+    return
+
+
+nearest_power_of_two(10)
+nearest_power_of_two(20)
+nearest_power_of_two(1)
+nearest_power_of_two(13)
