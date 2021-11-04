877218179674b211e8ce511f666f3bd609061b90
Tatiana Smatova <smatova.tatiana@gmail.com>
2021-11-04 17:09:48 +0300

    HomeWork 5: Task 5. Solution by bit operations

--- src/homework5/task5.py
+++ src/homework5/task5.py
@@ -1,18 +1,20 @@
 # Написать программу которая находит ближайшую степень двойки к введенному числу.
 # 10(8), 20(16), 1(1), 13(16)
 
-def nearest_power_of_two(n):
+def nearest_power_of_two_bit(number):
     i = 1
-    while n >= 2 ** i:
+    nearest_power = number
+    while nearest_power != 1:
+        nearest_power = nearest_power >> 1
         i += 1
-    if abs(n - (2 ** (i - 1))) <= abs(n - 2 ** i):
+    if abs(number - (2 ** (i - 1))) <= abs(number - 2 ** i):
         print(2 ** (i - 1))
     else:
         print(2 ** i)
     return
 
 
-nearest_power_of_two(10)
-nearest_power_of_two(20)
-nearest_power_of_two(1)
-nearest_power_of_two(13)
+nearest_power_of_two_bit(10)
+nearest_power_of_two_bit(20)
+nearest_power_of_two_bit(1)
+nearest_power_of_two_bit(13)
