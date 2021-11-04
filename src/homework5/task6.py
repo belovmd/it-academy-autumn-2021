a42646ad4ac38afebf766f0af12a79c4b2f5117f
Tatiana Smatova <smatova.tatiana@gmail.com>
2021-11-04 18:16:14 +0300

    HomeWork 5: Task 6

---
+++ src/homework5/task6.py
@@ -0,0 +1,16 @@
+# Вводится число найти его максимальный делитель, являющийся степенью двойки.
+# 10(2) 16(16), 12(4).
+
+def max_divider(n):
+    i = 0
+    divider = 1
+    while not n % divider:
+        divider = divider << 1
+        i += 1
+    divider = 2 ** (i-1)
+    return print(divider)
+
+
+max_divider(10)
+max_divider(16)
+max_divider(12)
