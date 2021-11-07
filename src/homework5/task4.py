e903e6b034faa91d213724f28292ea83366f601e
lenakotishova <86805388+lenakotishova@users.noreply.github.com>
2021-11-08 01:32:04 +0300

    Update task4.py

---
+++ src/homework5/task4.py
@@ -0,0 +1,72 @@
+import matplotlib.pyplot as plt
+
+
+with open(r'D:\python\it-academy-autumn-2021\src\homework5\data_hw5\ratings.list', 'r') as fh:
+    try:
+        list_file = fh.readlines()
+        i = req_line = 0
+        target_list = []
+
+        # Найдите ТОП250 фильмов и извлеките заголовки.
+        for line in list_file:
+            i += 1
+            if 'note: for this top 250, only votes from regular voters are considered.' in line:
+                print(line, i)
+
+            if 28 < i < 279:
+                target_list.append(line.strip())
+        for el in range(len(target_list)):
+            target_list[el] = target_list[el].split()
+        print(target_list)
+        TOP250 = []
+        for line in range(len(target_list)):
+            TOP250.append(target_list[line][3:-1])
+            TOP250[line] = ' '.join(TOP250[line])
+
+        # Программа создает 3 файла
+        # top250_movies.txt – названия файлов,
+
+        with open(r'D:\python\it-academy-autumn-2021\src\homework5\data_hw5\top250_movies.txt', 'w') as top_movies:
+            i = 1
+            for movie in TOP250:
+                top_movies.write(str(i) + '.' + ' ' + str(movie) + '\n')
+                i += 1
+
+        # ratings.txt – гистограмма рейтингов,
+        ratings = []
+        for el in range(len(target_list)):
+            ratings.append(target_list[el][2])
+        print(ratings)
+
+        plt.hist(ratings, bins=50,
+                 histtype='bar',
+                 color='blue',
+                 )
+
+        plt.xticks(rotation=45, fontsize=3)
+
+        plt.title('Ratings',
+                  fontweight="bold")
+
+        plt.savefig('ratings.png')
+
+        # years.txt – гистограмма годов.
+        years = []
+        for el in range(len(target_list)):
+            years.append(target_list[el][-1])
+        print(years)
+
+        plt.hist(years, bins=150,
+                 histtype='bar',
+                 color='blue',
+                 )
+
+        plt.xticks(rotation=45, fontsize=3)
+
+        plt.title('Years',
+                  fontweight="bold")
+
+        plt.savefig('years.png')
+
+    except FileNotFoundError:
+        print('Не существует')
