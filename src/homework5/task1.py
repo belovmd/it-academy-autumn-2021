09b5274e8d469432fdf62c1b1bd3f1dcd4a91e3c
Igorsuper <igor03111993@mail.ru>
2021-11-08 22:25:12 +0300

    131234

--- src/homework5/task1.py
+++ src/homework5/task1.py
@@ -5,16 +5,20 @@ a. runner() – все фукнции вызываются по очереди
 b. runner(‘func_name’) – вызывается только функцию func_name.
 c. runner(‘func’, ‘func1’...) - вызывает все переданные функции '''
 
+
 def text():
     a = "i love   Milk or   git or python"
     print(len(set(a.split())))
+
 
 def qwe():
     lst = [0, 3, 8, 0, 0, 3, 2, 0]
     lst1 = [x for x in lst if x] + [0] * lst.count(0)
     print(lst1)
 
+
 l_func = (text, qwe)
+
 
 def runner(*args):
     a = ""
@@ -25,5 +29,6 @@ def runner(*args):
         for func in l_func:
             a += str(func())
     return a
+
 
 runner(qwe)
